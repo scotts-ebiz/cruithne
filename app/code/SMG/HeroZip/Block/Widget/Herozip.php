85d925a6ccae3243cd53aa5ca74ad567dc3569cb
Ian Gall <ian.gall@scotts.com>
2018-09-21 14:00:09 -0400

    Adding php and phtml files

---
+++ app/code/SMG/HeroZip/Block/Widget/Herozip.php
@@ -0,0 +1,11 @@
+<?php
+namespace SMG\HeroZip\Block\Widget;
+
+use Magento\Framework\View\Element\Template;
+use Magento\Widget\Block\BlockInterface;
+
+class Herozip extends Template implements BlockInterface {
+
+  protected $_template = "widget/herozip.phtml";
+
+}
