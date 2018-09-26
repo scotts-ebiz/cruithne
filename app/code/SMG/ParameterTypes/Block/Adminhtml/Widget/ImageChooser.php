07fad2e130282702a6848e7a19297778976d99ca
Nathan VanHoose <nathan.vanhoose@scotts.com>
2018-09-26 10:37:03 -0400

    ECOM-25: incremental commit

--- app/code/SMG/ParameterTypes/Block/Adminhtml/Widget/ImageChooser.php
+++ app/code/SMG/ParameterTypes/Block/Adminhtml/Widget/ImageChooser.php
@@ -34,7 +34,22 @@ class ImageChooser extends Template {
      * @return Element
      */
     public function prepareElementHtml(Element $element) {
-        //$config = $this->_getData('config');
+        // this will get the desired preview html if the image
+        // already exists
+        $previewHtml = $this->getPreviewHtml($element);
+
+        // this will get the input text box for the image
+        /** @var \Magento\Framework\Data\Form\Element\Text $input */
+        $input = $this->elementFactory->create("text", ['data' => $element->getData()]);
+        $input->setId($element->getId());
+        $input->setForm($element->getForm());
+        $input->setClass("widget-option input-text admin__control-text");
+
+        if ($element->getRequired()) {
+            $input->addClass('required-entry');
+        }
+
+        // this will get the choose button
         $prefix = $element->getForm()->getHtmlIdPrefix();
         $elementId = $prefix . $element->getId();
 
@@ -48,18 +63,47 @@ class ImageChooser extends Template {
             ->setOnClick('MediabrowserUtility.openDialog(\''. $sourceUrl .'\')')
             ->setDisabled($element->getReadonly());
 
-        /** @var \Magento\Framework\Data\Form\Element\Text $input */
-        $input = $this->elementFactory->create("text", ['data' => $element->getData()]);
-        $input->setId($element->getId());
-        $input->setForm($element->getForm());
-        $input->setClass("widget-option input-text admin__control-text");
+        // this will get the delete button
+        $removeButton = $this->getLayout()->createBlock(\Magento\Backend\Block\Widget\Button::class)
+            ->setType('button')
+            ->setClass('btn-delete')
+            ->setLabel(__('Remove Image'))
+            ->setOnclick('document.getElementById(\''. $elementId .'\').value=\'\';if(document.getElementById(\''. $elementId .'_image\'))document.getElementById(\''. $elementId .'_image\').parentNode.remove()')
+            ->setDisabled($element->getReadonly());
 
-        if ($element->getRequired()) {
-            $input->addClass('required-entry');
+        // add to the after html element
+        $element->setData('after_element_html', '<div class="img_chooser_control">' . $input->getElementHtml() . $previewHtml . $chooser->toHtml() . $removeButton->toHtml() . "</div>");
+
+        // return
+        return $element;
+    }
+
+    /**
+     * Gets the preview html to display a preview the image
+     * that was selected.
+     *
+     * @param Element $element
+     * @return string
+     */
+    private function getPreviewHtml(Element $element) {
+        $previewHtml = '';
+        if ($element->getEscapedValue()) {
+            // Add image preview.
+            $url = $element->getEscapedValue();
+
+            if( !preg_match("/^http\:\/\/|https\:\/\//", $url) ) {
+                $url = Mage::getBaseUrl('media') . $url;
+            }
+
+            $previewHtml = '<a href="' . $url . '"'
+                . ' onclick="imagePreview(\'' . $element->getHtmlId() . '_image\'); return false;">'
+                . '<img src="' . $url . '" id="' . $element->getHtmlId() . '_image" title="' . $element->getEscapedValue() . '"'
+                . ' alt="' . $element->getEscapedValue() . '" height="40" class="small-image-preview v-middle"'
+                . ' style="margin-top:7px; border:1px solid grey" />'
+                . '</a> ';
         }
 
-        $element->setData('after_element_html', $input->getElementHtml() . $chooser->toHtml());
-
-        return $element;
+        // return
+        return $previewHtml;
     }
 }
