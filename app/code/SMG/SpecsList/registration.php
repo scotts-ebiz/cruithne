acce699a8d75f9f853d998179ce0945d17bdfdaa
hoose145 <nathan.vanhoose@scotts.com>
2018-10-15 08:54:40 -0400

    Ecom 25 mygro home page (#24)
    * ECOM-25: Made a widget for MyGro.
    
    * ECOM-25: Added Image parameter type and updated HeroCallout Widget.
    
    * ECOM-25: incremental commit
    
    * ECOM-25: Added mygro to the new install setup.
    
    * ECOM-25: Updated common used image chooser to work properly.
    
    * ECON-25: added jetpack widgets.
    
    * ECOM-25: Unregistered all newly added widgets until tested.  Add ProductGrid widget.
    
    * ECOM-25: Updated the mygro certificate for build process.
    
    * ECOM-25: Updated the mygro certificate for build process.
    
    * ECOM-25: Added Product Grid, Hero Video, and Features List widgets.
    
    * ECOM-25: Added widgets HeroZip, ProductCalloutText, ProductCalloutVideo.

--- app/code/SMG/SpecsList/registration.php
+++ app/code/SMG/SpecsList/registration.php
@@ -1,6 +1,6 @@
 <?php
-//\Magento\Framework\Component\ComponentRegistrar::register(
-//    \Magento\Framework\Component\ComponentRegistrar::MODULE,
-//    'SMG_SpecsList',
-//    __DIR__
-//);
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'SMG_SpecsList',
+    __DIR__
+);
