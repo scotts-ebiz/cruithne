3b0c2e48bb516600dd5bf372e0e80e3f8a5e65c8
hoose145 <nathan.vanhoose@scotts.com>
2018-10-25 09:40:41 -0400

    Ecom 17 scottsprogram widget (#30)
    * ECOM-25: Made a widget for MyGro.
    
    * Adding initial files for Hero Zip
    
    * Adding php and phtml files
    
    * Added widget xml
    
    * Added blck for textarea
    
    * Added files for the textarea
    
    * Removed my approach to the textarea by deleting file
    
    * ECOM-25: Added Image parameter type and updated HeroCallout Widget.
    
    * ECOM-25: incremental commit
    
    * ECOM-25: Added mygro to the new install setup.
    
    * ECOM-25: Updated common used image chooser to work properly.
    
    * ECON-25: added jetpack widgets.
    
    * Changes for the widget
    
    * ECOM-17: Added ZipCodeList Widget.
    
    * ECOM-17: Mdae changes to the Landing Page and ZipCodeList and added new controller for processing zip codes.

--- app/code/SMG/LandingPage/Block/Widget/Landingpage.php
+++ app/code/SMG/LandingPage/Block/Widget/Landingpage.php
@@ -3,8 +3,58 @@
 namespace SMG\LandingPage\Block\Widget;
 
 use Magento\Framework\View\Element\Template;
+use Magento\Framework\View\Element\Template\Context;
 use Magento\Widget\Block\BlockInterface;
+use Magento\Catalog\Api\ProductRepositoryInterface;
+use Magento\Store\Model\StoreManagerInterface;
+use Magento\Framework\Data\Form\FormKey;
 
-class Landingpage extends Template implements BlockInterface {
+class Landingpage extends Template implements BlockInterface
+{
     protected $_template = "widget/landingpage.phtml";
+    protected $_productRepository;
+    protected $_storeManager;
+    protected $_formKey;
+
+    public function __construct(Context $context, ProductRepositoryInterface $productRepository, StoreManagerInterface $storeManager, FormKey $formKey, array $data = [])
+    {
+        $this->_productRepository = $productRepository;
+        $this->_storeManager = $storeManager;
+        $this->_formKey = $formKey;
+
+        parent::__construct($context, $data);
+    }
+
+    /**
+     * Get the Product object from the id
+     *
+     * @param $id
+     * @return \Magento\Catalog\Api\Data\ProductInterface
+     * @throws \Magento\Framework\Exception\NoSuchEntityException
+     */
+    public function getLoadProduct($id)
+    {
+        return $this->_productRepository->getById($id);
+    }
+
+    /**
+     * Get the Product Image Url
+     *
+     * @param $image
+     * @return string
+     */
+    public function getMediaBaseUrl($image)
+    {
+        return $this->_baseUrl . 'pub/media/catalog/product/' . $image;
+    }
+
+    /**
+     * Get the auto generated form key
+     *
+     * @return mixed
+     */
+    public function getFormKey()
+    {
+        return $this->formKey->getFormKey();
+    }
 }
--- app/code/SMG/Zip/registration.php
+++ app/code/SMG/Zip/registration.php
@@ -1,6 +1,6 @@
 <?php
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'SMG_LandingPage',
+    'SMG_Zip',
     __DIR__
 );
