2bc7cf33e8decec91d1995337d5a20a2e006f1e8
hoose145 <nathan.vanhoose@scotts.com>
2018-11-13 14:57:07 -0500

    Revert "Ecom 25 mygro home page (#43)" (#44)
    This reverts commit e50a0db2f50ea1b445aab3b3bd2b5d65e6680a4f.

---
+++ app/code/SMG/ParameterTypes/Block/Adminhtml/Widget/Wysiwyg.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace SMG\ParameterTypes\Block\Adminhtml\Widget\Type;
+
+use \Magento\Backend\Block\Template;
+use \Magento\Backend\Block\Template\Context;
+use \Magento\Framework\Data\Form\Element\Factory;
+use \Magento\Cms\Model\Wysiwyg\Config;
+use \Magento\Framework\Data\Form\Element\AbstractElement;
+
+Class Wysiwyg extends Template
+{
+    /**
+     * @var \Magento\Cms\Model\Wysiwyg\Config
+     */
+    protected $wysiwygConfig;
+
+    /**
+     * @var \Magento\Framework\Data\Form\Element\Factory
+     */
+    protected $factoryElement;
+
+    /**
+     * @param \Magento\Backend\Block\Template\Context $context
+     * @param \Magento\Framework\Data\Form\Element\Factory $factoryElement
+     * @param \Magento\Cms\Model\Wysiwyg\Config $wysiwygConfig
+     * @param array $data
+     */
+    public function __construct(Context $context, Factory $factoryElement, Config $wysiwygConfig, $data = []) {
+        $this->factoryElement = $factoryElement;
+
+        $this->wysiwygConfig = $wysiwygConfig;
+
+        parent::__construct($context, $data);
+    }
+
+    /**
+     * Prepare chooser element HTML
+     *
+     * @param \Magento\Framework\Data\Form\Element\AbstractElement $element Form Element
+     * @return \Magento\Framework\Data\Form\Element\AbstractElement
+     */
+    public function prepareElementHtml(AbstractElement $element) {
+        $editor = $this->factoryElement->create('editor', ['data' => $element->getData()])
+            ->setLabel('')
+            ->setForm($element->getForm())
+            ->setWysiwyg(true)
+            ->setConfig(
+                $this->wysiwygConfig->getConfig([
+                    'add_variables' => false,
+                    'add_widgets' => false,
+                    'add_images' => false
+                ])
+            );
+
+        if ($element->getRequired()) {
+            $editor->addClass('required-entry');
+        }
+
+        $element->setData('after_element_html', $editor->getElementHtml());
+
+        // return
+        return $element;
+    }
+}
