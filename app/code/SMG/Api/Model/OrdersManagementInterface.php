98bfb28fd120dc4f27eed1ebd86d0aff32f769a4
Nathan VanHoose <nathan.vanhoose@scotts.com>
2018-11-27 14:22:21 -0500

    ECOM-149: Updated code to retrieve the orders from an API call.

--- app/code/SMG/Api/Model/OrdersManagementInterface.php
+++ app/code/SMG/Api/Model/OrdersManagementInterface.php
@@ -5,14 +5,9 @@ namespace SMG\Api\Model;
 interface OrdersManagementInterface
 {
     /**
-     * This function will get the orders in the following type
-     * format.
+     * This function will get the orders in a JSON format.
      *
-     * XML
-     * JSON
-     * CSV
-     *
-     * @return mixed
+     * @return string
      */
     public function getOrders();
 }
