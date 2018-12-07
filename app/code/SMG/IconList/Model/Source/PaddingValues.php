006f1f7d70ffdfd1450dbcea9a93079033f22349
Ian Gall <ian.gall@scotts.com>
2018-12-07 16:19:06 -0500

    Adding files for Icon List controls

---
+++ app/code/SMG/IconList/Model/Source/PaddingValues.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace SMG\IconList\Model\Source;
+
+class PaddingValues implements \Magento\Framework\Option\ArrayInterface
+{
+  public function toOptionArray()
+  {
+    return [
+      ['value' => 'padding-bottom: 0%; ', 'label' => __('0%')],
+      ['value' => 'padding-bottom: 1%; ', 'label' => __('1%')],
+      ['value' => 'padding-bottom: 2%; ', 'label' => __('2%')],
+      ['value' => 'padding-bottom: 3%; ', 'label' => __('3%')],
+      ['value' => 'padding-bottom: 4%; ', 'label' => __('4%')],
+      ['value' => 'padding-bottom: 5%; ', 'label' => __('5%')],
+      ['value' => 'padding-bottom: 6%; ', 'label' => __('6%')],
+      ['value' => 'padding-bottom: 7%; ', 'label' => __('7%')],
+      ['value' => 'padding-bottom: 8%; ', 'label' => __('8%')],
+      ['value' => 'padding-bottom: 9%; ', 'label' => __('9%')],
+      ['value' => 'padding-bottom: 10%; ', 'label' => __('10%')],
+      ['value' => 'padding-bottom: 11%; ', 'label' => __('11%')],
+      ['value' => 'padding-bottom: 12%; ', 'label' => __('12%')],
+      ['value' => 'padding-bottom: 13%; ', 'label' => __('13%')],
+      ['value' => 'padding-bottom: 14%; ', 'label' => __('14%')],
+      ['value' => 'padding-bottom: 15%; ', 'label' => __('15%')],
+      ['value' => 'padding-bottom: 16%; ', 'label' => __('16%')],
+      ['value' => 'padding-bottom: 17%; ', 'label' => __('17%')],
+      ['value' => 'padding-bottom: 18%; ', 'label' => __('18%')],
+      ['value' => 'padding-bottom: 19%; ', 'label' => __('19%')],
+      ['value' => 'padding-bottom: 20%; ', 'label' => __('20%')],
+    ];
+  }
+}
