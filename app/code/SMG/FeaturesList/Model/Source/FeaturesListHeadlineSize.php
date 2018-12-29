19a4872326ae86ebf9f7217442c67e39d297dae0
Ian Gall <ian.gall@scotts.com>
2018-12-29 13:46:59 -0500

    Altered files for customization

--- app/code/SMG/FeaturesList/Model/Source/FeaturesListHeadlineSize.php
+++ app/code/SMG/FeaturesList/Model/Source/FeaturesListHeadlineSize.php
@@ -7,17 +7,17 @@ class FeaturesListHeadlineSize implements \Magento\Framework\Option\ArrayInterfa
   public function toOptionArray()
   {
     return [
-        ['value' => 'font-size: 1.2rem; ', 'label' => __('1.2')],
-        ['value' => 'font-size: 1.3rem; ', 'label' => __('1.3')],
-        ['value' => 'font-size: 1.4rem; ', 'label' => __('1.4')],
-        ['value' => 'font-size: 1.5rem; ', 'label' => __('1.5')],
-        ['value' => 'font-size: 1.6rem; ', 'label' => __('1.6')],
-        ['value' => 'font-size: 1.7rem; ', 'label' => __('1.7')],
-        ['value' => 'font-size: 1.8rem; ', 'label' => __('1.8')],
-        ['value' => 'font-size: 1.9rem; ', 'label' => __('1.9')],
         ['value' => 'font-size: 2.0rem; ', 'label' => __('2.0')],
         ['value' => 'font-size: 2.1rem; ', 'label' => __('2.1')],
-        ['value' => 'font-size: 2.2rem; ', 'label' => __('2.2')]
+        ['value' => 'font-size: 2.2rem; ', 'label' => __('2.2')],
+        ['value' => 'font-size: 2.3rem; ', 'label' => __('2.3')],
+        ['value' => 'font-size: 2.4rem; ', 'label' => __('2.4')],
+        ['value' => 'font-size: 2.5rem; ', 'label' => __('2.5')],
+        ['value' => 'font-size: 2.6rem; ', 'label' => __('2.6')],
+        ['value' => 'font-size: 2.7rem; ', 'label' => __('2.7')],
+        ['value' => 'font-size: 2.8rem; ', 'label' => __('2.8')],
+        ['value' => 'font-size: 2.9rem; ', 'label' => __('2.9')],
+        ['value' => 'font-size: 3.0rem; ', 'label' => __('3.0')]
     ];
   }
 }
