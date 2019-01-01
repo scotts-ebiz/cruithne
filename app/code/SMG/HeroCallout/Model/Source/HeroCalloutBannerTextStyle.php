35220622e3fbb5d2ecce2e239b942584ec268724
Ian Gall <ian.gall@scotts.com>
2019-01-01 16:45:41 -0500

    Adding style files

--- app/code/SMG/HeroCallout/Model/Source/HeroCalloutBannerTextStyle.php
+++ app/code/SMG/HeroCallout/Model/Source/HeroCalloutBannerTextStyle.php
@@ -2,7 +2,7 @@
 
 namespace SMG\HeroCallout\Model\Source;
 
-class HeroCalloutBannerHeadlineStyles implements \Magento\Framework\Option\ArrayInterface
+class HeroCalloutBannerTextStyle implements \Magento\Framework\Option\ArrayInterface
 {
   public function toOptionArray()
   {
