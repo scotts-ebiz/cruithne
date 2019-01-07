f1ba7e4edb072ff1047ff5e6a058d05542e7a46f
Ian Gall <ian.gall@scotts.com>
2019-01-07 15:22:00 -0500

    Adding new file

---
+++ app/code/SMG/ProductGrid/Model/Source/productButtonTextSize.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace SMG\ProductGrid\Model\Source;
+
+class productButtonTextSize implements \Magento\Framework\Option\ArrayInterface
+{
+  public function toOptionArray()
+  {
+    return [
+        ['value' => 'font-size: 1.0rem; ', 'label' => __('1.0')],
+        ['value' => 'font-size: 1.1rem; ', 'label' => __('1.1')],
+        ['value' => 'font-size: 1.2rem; ', 'label' => __('1.2')],
+        ['value' => 'font-size: 1.3rem; ', 'label' => __('1.3')],
+        ['value' => 'font-size: 1.4rem; ', 'label' => __('1.4')],
+        ['value' => 'font-size: 1.5rem; ', 'label' => __('1.5')],
+        ['value' => 'font-size: 1.6rem; ', 'label' => __('1.6')],
+        ['value' => 'font-size: 1.7rem; ', 'label' => __('1.7')],
+        ['value' => 'font-size: 1.8rem; ', 'label' => __('1.8')],
+        ['value' => 'font-size: 1.9rem; ', 'label' => __('1.9')],
+        ['value' => 'font-size: 2.0rem; ', 'label' => __('2.0')],
+        ['value' => 'font-size: 2.1rem; ', 'label' => __('2.1')],
+        ['value' => 'font-size: 2.2rem; ', 'label' => __('2.2')],
+        ['value' => 'font-size: 2.3rem; ', 'label' => __('2.3')],
+        ['value' => 'font-size: 2.4rem; ', 'label' => __('2.4')],
+        ['value' => 'font-size: 2.5rem; ', 'label' => __('2.5')]
+    ];
+  }
+}
