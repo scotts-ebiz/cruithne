1534fbefa43c80e455ed92d393060e24ae87a1fc
Ian Gall <ian.gall@scotts.com>
2019-01-07 16:21:12 -0500

    Adding new files for button styles

--- app/code/SMG/ProductGrid/Model/Source/productButtonTextSize.php
+++ app/code/SMG/ProductGrid/Model/Source/productButtonTextSize.php
@@ -7,6 +7,11 @@ class productButtonTextSize implements \Magento\Framework\Option\ArrayInterface
   public function toOptionArray()
   {
     return [
+        ['value' => 'font-size: 0.5rem; ', 'label' => __('0.5')],
+        ['value' => 'font-size: 0.6rem; ', 'label' => __('0.6')],
+        ['value' => 'font-size: 0.7rem; ', 'label' => __('0.7')],
+        ['value' => 'font-size: 0.8rem; ', 'label' => __('0.8')],
+        ['value' => 'font-size: 0.9rem; ', 'label' => __('0.9')],
         ['value' => 'font-size: 1.0rem; ', 'label' => __('1.0')],
         ['value' => 'font-size: 1.1rem; ', 'label' => __('1.1')],
         ['value' => 'font-size: 1.2rem; ', 'label' => __('1.2')],
@@ -17,12 +22,7 @@ class productButtonTextSize implements \Magento\Framework\Option\ArrayInterface
         ['value' => 'font-size: 1.7rem; ', 'label' => __('1.7')],
         ['value' => 'font-size: 1.8rem; ', 'label' => __('1.8')],
         ['value' => 'font-size: 1.9rem; ', 'label' => __('1.9')],
-        ['value' => 'font-size: 2.0rem; ', 'label' => __('2.0')],
-        ['value' => 'font-size: 2.1rem; ', 'label' => __('2.1')],
-        ['value' => 'font-size: 2.2rem; ', 'label' => __('2.2')],
-        ['value' => 'font-size: 2.3rem; ', 'label' => __('2.3')],
-        ['value' => 'font-size: 2.4rem; ', 'label' => __('2.4')],
-        ['value' => 'font-size: 2.5rem; ', 'label' => __('2.5')]
+        ['value' => 'font-size: 2.0rem; ', 'label' => __('2.0')]
     ];
   }
 }
