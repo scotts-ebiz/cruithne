2a0941ae290297cbcc3d62072dfe9d3d323ddfd5
Ian Gall <ian.gall@scotts.com>
2019-01-07 14:14:42 -0500

    Changing available sizes

--- app/code/SMG/ProductGrid/Model/Source/productHeadlineTextSize.php
+++ app/code/SMG/ProductGrid/Model/Source/productHeadlineTextSize.php
@@ -7,6 +7,11 @@ class productHeadlineTextSize implements \Magento\Framework\Option\ArrayInterfac
   public function toOptionArray()
   {
     return [
+        ['value' => 'font-size: 1.0rem; ', 'label' => __('1.0')],
+        ['value' => 'font-size: 1.1rem; ', 'label' => __('1.1')],
+        ['value' => 'font-size: 1.2rem; ', 'label' => __('1.2')],
+        ['value' => 'font-size: 1.3rem; ', 'label' => __('1.3')],
+        ['value' => 'font-size: 1.4rem; ', 'label' => __('1.4')],
         ['value' => 'font-size: 1.5rem; ', 'label' => __('1.5')],
         ['value' => 'font-size: 1.6rem; ', 'label' => __('1.6')],
         ['value' => 'font-size: 1.7rem; ', 'label' => __('1.7')],
@@ -17,10 +22,7 @@ class productHeadlineTextSize implements \Magento\Framework\Option\ArrayInterfac
         ['value' => 'font-size: 2.2rem; ', 'label' => __('2.2')],
         ['value' => 'font-size: 2.3rem; ', 'label' => __('2.3')],
         ['value' => 'font-size: 2.4rem; ', 'label' => __('2.4')],
-        ['value' => 'font-size: 2.5rem; ', 'label' => __('2.5')],
-        ['value' => 'font-size: 2.6rem; ', 'label' => __('2.6')],
-        ['value' => 'font-size: 2.7rem; ', 'label' => __('2.7')],
-        ['value' => 'font-size: 2.8rem; ', 'label' => __('2.8')]
+        ['value' => 'font-size: 2.5rem; ', 'label' => __('2.5')]
     ];
   }
 }
