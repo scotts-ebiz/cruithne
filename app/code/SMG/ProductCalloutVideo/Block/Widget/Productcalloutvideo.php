1007bbaf9aaa440cf2e43627f44b7029cd5b677f
Ian Gall <ian.gall@scotts.com>
2019-01-11 09:55:50 -0500

    Changig how the ProductCalloutVideo is called

--- app/code/SMG/ProductCalloutVideo/Block/Widget/Productcalloutvideo.php
+++ app/code/SMG/ProductCalloutVideo/Block/Widget/Productcalloutvideo.php
@@ -5,6 +5,11 @@ namespace SMG\ProductCalloutVideo\Block\Widget;
 use Magento\Framework\View\Element\Template;
 use Magento\Widget\Block\BlockInterface;
 
-class Productcalloutvideo extends Template implements BlockInterface {
-    protected $_template = "widget/productcalloutvideo.phtml";
+class Productcalloutvideo extends \Magento\Framework\View\Element\Template {
+    //protected $_template = "widget/productcalloutvideo.phtml";
+
+  protected function _toHtml() {
+    $html = 'This is a test!';
+    return $html;
+  }
 }
