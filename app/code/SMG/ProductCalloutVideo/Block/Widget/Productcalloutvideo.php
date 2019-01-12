c08d9c846913cdd3afa77d75be2e014a840b1526
Ian Gall <ian.gall@scotts.com>
2019-01-12 14:57:02 -0500

    Made changes to pull in the videoEmbedCode var

--- app/code/SMG/ProductCalloutVideo/Block/Widget/Productcalloutvideo.php
+++ app/code/SMG/ProductCalloutVideo/Block/Widget/Productcalloutvideo.php
@@ -2,14 +2,20 @@
 
 namespace SMG\ProductCalloutVideo\Block\Widget;
 
+//Creates a shorthand for that you can just use Template in the statement below
 use Magento\Framework\View\Element\Template;
+
+//Creates a shorthand for that you can just use BlockInterface in the statement below
 use Magento\Widget\Block\BlockInterface;
 
-class Productcalloutvideo extends \Magento\Framework\View\Element\Template {
-    //protected $_template = "widget/productcalloutvideo.phtml";
+
+
+class Productcalloutvideo extends Template implements BlockInterface {
+
 
   protected function _toHtml() {
-    $html = 'This is a test!';
+    $html = '<section class="product-callout-video-widget product-info-container align-top">' . trim($this->getData
+      ('videoembedcode')) . '<div class="wenis">qwerqwer</div>' . '</section>';
     return $html;
   }
 }
