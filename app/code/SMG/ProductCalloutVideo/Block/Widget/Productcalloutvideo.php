134d8d9a9ae3e37281dd29db6d353c15272bb051
Ian Gall <ian.gall@scotts.com>
2019-01-12 14:59:57 -0500

    I had to revert back to using the pthml file

--- app/code/SMG/ProductCalloutVideo/Block/Widget/Productcalloutvideo.php
+++ app/code/SMG/ProductCalloutVideo/Block/Widget/Productcalloutvideo.php
@@ -2,20 +2,9 @@
 
 namespace SMG\ProductCalloutVideo\Block\Widget;
 
-//Creates a shorthand for that you can just use Template in the statement below
 use Magento\Framework\View\Element\Template;
-
-//Creates a shorthand for that you can just use BlockInterface in the statement below
 use Magento\Widget\Block\BlockInterface;
 
-
-
 class Productcalloutvideo extends Template implements BlockInterface {
-
-
-  protected function _toHtml() {
-    $html = '<section class="product-callout-video-widget product-info-container align-top">' . trim($this->getData
-      ('videoembedcode')) . '<div class="wenis">qwerqwer</div>' . '</section>';
-    return $html;
-  }
+  protected $_template = "widget/productcalloutvideo.phtml";
 }
