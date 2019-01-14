6c7efe74316abab595717fd3822f9cdee4198351
Ivchenko Igor <igor.ivchenko@classyllama.com>
2019-01-14 13:23:39 +0200

    feature/SMG-610_14_stop_order_customer_service_email

--- app/code/SMG/CustomerServiceEmail/Api/OrderManagementInterface.php
+++ app/code/SMG/CustomerServiceEmail/Api/OrderManagementInterface.php
@@ -21,10 +21,18 @@ interface OrderManagementInterface
     public function notify($id);
 
     /**
-     * Failed capture transaction emails a specified order.
+     * Failed capture transaction email a specified order.
      *
      * @param int $id The order ID.
      * @return bool
      */
     public function notifyServiceTeam($id);
+
+    /**
+     * Failed capture transaction emails a specified orders.
+     *
+     * @param \SMG\CustomerServiceEmail\Api\Data\ItemInterface $item
+     * @return bool
+     */
+    public function notifyEmailsServiceTeam(\SMG\CustomerServiceEmail\Api\Data\ItemInterface $item);
 }
--- app/code/SMG/CustomerServiceEmail/Model/Service/OrderService.php
+++ app/code/SMG/CustomerServiceEmail/Model/Service/OrderService.php
@@ -8,7 +8,10 @@ namespace SMG\CustomerServiceEmail\Model\Service;
 use SMG\CustomerServiceEmail\Api\OrderManagementInterface;
 use SMG\CustomerServiceEmail\Model\OrderCancelNotifier;
 use SMG\CustomerServiceEmail\Model\FailedTransactionNotifier;
+use SMG\CustomerServiceEmail\Api\Data\ItemInterface;
 use Magento\Sales\Api\OrderRepositoryInterface;
+use Magento\Framework\Api\SearchCriteriaBuilder;
+use Magento\Sales\Model\Order;
 
 /**
  * Class OrderService
@@ -32,18 +35,26 @@ class OrderService implements OrderManagementInterface
     private $orderRepository;
 
     /**
+     * @var SearchCriteriaBuilder
+     */
+    private $searchCriteriaBuilder;
+
+    /**
      * @param OrderCancelNotifier $notifier
      * @param FailedTransactionNotifier $failedTransactionNotifier
      * @param OrderRepositoryInterface $orderRepository
+     * @param SearchCriteriaBuilder $searchCriteria
      */
     public function __construct(
         OrderCancelNotifier $notifier,
         FailedTransactionNotifier $failedTransactionNotifier,
-        OrderRepositoryInterface $orderRepository
+        OrderRepositoryInterface $orderRepository,
+        SearchCriteriaBuilder $searchCriteria
     ) {
         $this->notifier = $notifier;
         $this->failedTransactionNotifier = $failedTransactionNotifier;
         $this->orderRepository = $orderRepository;
+        $this->searchCriteriaBuilder = $searchCriteria;
     }
 
     /**
@@ -54,19 +65,49 @@ class OrderService implements OrderManagementInterface
      */
     public function notify($id)
     {
+        /** @var Order $order */
         $order = $this->orderRepository->get($id);
         return $this->notifier->notify($order);
     }
 
     /**
-     * Failed capture transaction emails a specified order.
+     * Failed capture transaction email a specified order.
      *
      * @param int $id The order ID.
      * @return bool
      */
     public function notifyServiceTeam($id)
     {
+        /** @var Order $order */
         $order = $this->orderRepository->get($id);
         return $this->failedTransactionNotifier->notify($order);
+    }
+
+    /**
+     * Failed capture transaction emails a specified orders.
+     *
+     * @param ItemInterface $item
+     * @return bool
+     */
+    public function notifyEmailsServiceTeam(ItemInterface $item)
+    {
+        $ids = $item->getOrderIds();
+
+        if (!empty($ids)) {
+            $orders = $this->orderRepository->getList(
+                $this->searchCriteriaBuilder
+                    ->addFilter('entity_id', $ids, 'in')
+                    ->create()
+            )->getItems();
+
+            /** @var Order $order */
+            foreach ($orders as $order) {
+                $this->failedTransactionNotifier->notify($order);
+            }
+
+            return true;
+        }
+
+        return false;
     }
 }
