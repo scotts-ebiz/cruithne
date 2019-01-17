6c83e3a3761f8052667a8c7a88278e825db5a557
Ivchenko Igor <igor.ivchenko@classyllama.com>
2019-01-17 17:40:35 +0200

    feature/SMG-610_23_config_expedited_shipping_prices

---
+++ app/code/SMG/OfflineShipping/Plugin/Model/Carrier.php
@@ -0,0 +1,115 @@
+<?php
+/**
+ * @copyright Copyright (c) 2019 SMG, LLC
+ */
+
+namespace SMG\OfflineShipping\Plugin\Model;
+
+use Magento\OfflineShipping\Model\Carrier\Flatrate;
+use Magento\Shipping\Model\Rate\Result;
+use Magento\Shipping\Model\Rate\ResultFactory;
+use Magento\Quote\Model\Quote\Address\RateResult\MethodFactory;
+use Magento\Quote\Model\Quote\Address\RateResult\Method;
+use SMG\OfflineShipping\Model\Config\Source\Method as SourceMethod;
+
+/**
+ * Class Carrier
+ * @package SMG\OfflineShipping\Plugin\Model
+ */
+class Carrier
+{
+    /**
+     * @var string
+     */
+    const CODE = 'flatrate';
+
+    /**
+     * @var string
+     */
+    const DEFAULT_TITLE = 'Flat Rate';
+
+    /**
+     * @var SourceMethod
+     */
+    private $sourceMethod;
+
+    /**
+     * @var ResultFactory
+     */
+    private $rateResultFactory;
+
+    /**
+     * @var MethodFactory
+     */
+    private $rateMethodFactory;
+
+    /**
+     * @param SourceMethod $sourceMethod
+     * @param ResultFactory $rateResultFactory
+     * @param MethodFactory $rateMethodFactory
+     */
+    public function __construct(
+        SourceMethod $sourceMethod,
+        ResultFactory $rateResultFactory,
+        MethodFactory $rateMethodFactory
+    ) {
+        $this->sourceMethod = $sourceMethod;
+        $this->rateResultFactory = $rateResultFactory;
+        $this->rateMethodFactory = $rateMethodFactory;
+    }
+
+    /**
+     * @param Flatrate $subject
+     * @param array $result
+     * @return array
+     */
+    public function afterGetAllowedMethods(
+        Flatrate $subject,
+        array $result
+    ) {
+        $allowedMethods = explode(",", $subject->getConfigData('allowed_methods'));
+        $availableMethods = $this->sourceMethod->getAvailableMethods();
+        $list = [];
+
+        foreach ($availableMethods as $code => $title) {
+            if (!in_array($code, $allowedMethods)) {
+                continue;
+            }
+
+            $list[$code] = $title;
+        }
+
+        return $list;
+    }
+    /**
+     * @param Flatrate $subject
+     * @param $result
+     * @return bool|Result
+     */
+    public function afterCollectRates(
+        Flatrate $subject,
+        $result
+    ) {
+        if (!$subject->getConfigFlag('active') || !$subject->getAllowedMethods()) {
+            return false;
+        }
+
+        $price = $result->getCheapestRate()->getPrice();
+        $cost = $result->getCheapestRate()->getCost();
+
+        foreach ($subject->getAllowedMethods() as $code => $name) {
+            /** @var Method $method */
+            $method = $this->rateMethodFactory->create();
+            $method->setCarrier(self::CODE);
+            $carrierTitle = $subject->getConfigData('title');
+            $method->setCarrierTitle($carrierTitle);
+            $method->setMethod($code);
+            $method->setMethodTitle($name);
+            $method->setPrice($price);
+            $method->setCost($cost);
+            $result->append($method);
+        }
+
+        return $result;
+    }
+}
