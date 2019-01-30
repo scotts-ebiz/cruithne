9337020f08a781ac7c9bb48915f26851c9f30bda
neelam <neelam.neemkar@scotts.com>
2019-01-30 01:00:25 -0500

    vantivcredit Ecomm-156

---
+++ app/code/SMG/Creditvantiv/Model/ResourceModel/Payment.php
@@ -0,0 +1,27 @@
+<?php
+namespace SMG\Creditvantiv\Model\ResourceModel;
+/**
+* Payment Resource Model
+*
+* @method \Creditvantiv\Payment\Model\Resource\Page _getResource()
+* @method \Creditvantiv\Payment\Model\Resource\Page getResource()
+*/
+class Payment extends \Magento\Framework\Model\ResourceModel\Db\AbstractDb
+{
+	/**
+	* Initialize resource model
+	*
+	* @return void
+	*/
+	public function __construct(
+		\Magento\Framework\Model\ResourceModel\Db\Context $context
+	)
+	{
+		parent::__construct($context);
+	}
+
+	protected function _construct(){
+		$this->_init('sales_order_credit_batch', 'entity_id');
+	}
+
+}
