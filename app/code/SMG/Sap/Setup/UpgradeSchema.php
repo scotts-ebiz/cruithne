8d97f28c5361d57bb51289d8fa1dc9c5641a7916
hoose145 <nathan.vanhoose@scotts.com>
2019-03-01 12:20:59 -0500

    Ecom 346 credit order (#173)
    * ECOM-346: Added code to take the credits and send them in the orders API.
    
    * Update OrdersHelper.php
    
    @travis-at-scotts 
    
    Good catch!  I have changed it.

--- app/code/SMG/Sap/Setup/UpgradeSchema.php
+++ app/code/SMG/Sap/Setup/UpgradeSchema.php
@@ -26,6 +26,11 @@ class UpgradeSchema implements UpgradeSchemaInterface
         {
             $this->updateColumnVersion120($setup);
         }
+
+        if (version_compare($context->getVersion(), '1.3.0', '<'))
+        {
+            $this->updateColumnVersion130($setup);
+        }
     }
 
     private function updateColumnVersion110(SchemaSetupInterface $setup)
@@ -70,5 +75,133 @@ class UpgradeSchema implements UpgradeSchemaInterface
 
         // end the setup
         $setup->endSetup();
+    }
+
+    private function updateColumnVersion130(SchemaSetupInterface $setup)
+    {
+        // start the setup
+        $setup->startSetup();
+
+        // update batch table
+        $this->updateSalesOrderSapBatch130($setup);
+
+        // create the new batch item table
+        $this->createSalesOrderSapBatchItem($setup);
+
+        // end the setup
+        $setup->endSetup();
+    }
+
+    private function updateSalesOrderSapBatch130(SchemaSetupInterface $setup)
+    {
+        $tableName = 'sales_order_sap_batch';
+
+        // make a new table with the desired table name
+        $setup->getConnection()->addColumn(
+            $tableName,
+            'is_order',
+            [
+                'type' => Table::TYPE_BOOLEAN,
+                'nullable' => false,
+                'default' => false,
+                'comment' => 'Flag for when and order has been sent to SAP'
+            ]
+        );
+
+        $setup->getConnection()->addColumn(
+            $tableName,
+            'order_process_date',
+            [
+                'type' => Table::TYPE_TIMESTAMP,
+                'nullable' => true,
+                'comment' => 'Datetime for when an order was sent to SAP'
+            ]
+        );
+    }
+
+    /**
+     * @param SchemaSetupInterface $setup
+     * @throws \Zend_Db_Exception
+     */
+    private function createSalesOrderSapBatchItem(SchemaSetupInterface $setup)
+    {
+        $tableName = 'sales_order_sap_batch_item';
+
+        // make a new table with the desired table name
+        $table = $setup->getConnection()->newTable($setup->getTable($tableName));
+
+        // add the desired columns
+        $table->addColumn(
+            'entity_id',
+            Table::TYPE_INTEGER,
+            null,
+            [
+                'primary' => true,
+                'auto_increment' => true,
+                'nullable' => false,
+                'unsigned' => true
+            ]
+        );
+
+        $table->addColumn(
+            'creditmemo_order_id',
+            Table::TYPE_INTEGER,
+            null,
+            [
+                'nullable' => false,
+                'unsigned' => true
+            ]
+        );
+
+        $table->addColumn(
+            'order_id',
+            Table::TYPE_INTEGER,
+            null,
+            [
+                'nullable' => false,
+                'unsigned' => true
+            ]
+        );
+
+        $table->addColumn(
+            'order_item_id',
+            Table::TYPE_INTEGER,
+            null,
+            [
+                'nullable' => false,
+                'unsigned' => true
+            ]
+        );
+
+        $table->addColumn(
+            'sku',
+            Table::TYPE_TEXT,
+            255,
+            [
+                'nullable' => false
+            ]
+        );
+
+        $table->addColumn(
+            'is_credit',
+            Table::TYPE_BOOLEAN,
+            null,
+            [
+                'nullable' => false,
+                'default' => false
+            ]
+        );
+
+        $table->addColumn(
+            'credit_process_date',
+            Table::TYPE_TIMESTAMP,
+            null,
+            [
+                'nullable' => true
+            ]
+        );
+
+        // create the table
+        $setup->getConnection()->createTable($table);
     }
 }
