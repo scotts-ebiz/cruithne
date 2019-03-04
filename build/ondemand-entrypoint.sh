85342e1c679ae97d4657750ff6d08b3d3a4008ef
Travis Casper <Travis.Casper@scotts.com>
2019-03-04 17:19:42 -0500

    added redis changes and commented out some of the commands that take a long time. Might need to include them later though.

--- build/ondemand-entrypoint.sh
+++ build/ondemand-entrypoint.sh
@@ -16,14 +16,18 @@ COMMAND="$@"
 
 # rm -rf /var/www/html/magento2/var/generation/*
 # su - magento -c '/var/www/html/magento2/bin/magento deploy:mode:set -s developer'
-su - magento -c '/var/www/html/magento2/bin/magento deploy:mode:set -s production'
-su - magento -c '/var/www/html/magento2/bin/magento setup:upgrade'
-su - magento -c '/var/www/html/magento2/bin/magento setup:di:compile'
+# su - magento -c '/var/www/html/magento2/bin/magento deploy:mode:set -s production'
+# su - magento -c '/var/www/html/magento2/bin/magento setup:upgrade'
+# su - magento -c '/var/www/html/magento2/bin/magento setup:di:compile'
+
+# TODO We should abstract the server address....
+su - magento -c 'php ./bin/magento setup:config:set --cache-backend=redis --cache-backend-redis-server=10.0.2.3   --cache-backend-redis-db=0'
+su - magento -c 'php ./bin/magento setup:config:set --page-cache=redis --page-cache-redis-server=10.0.2.3 --page-cache-redis-db=1'
+su - magento -c 'php ./bin/magento setup:config:set --session-save=redis --session-save-redis-host=10.0.2.3 --session-save-redis-log-level=3 --session-save-redis-db=2 -q'
+
 su - magento -c 'gulp clean -f /var/www/html/magento2/tools/gulpfile.js'
 su - magento -c 'gulp styles -f /var/www/html/magento2/tools/gulpfile.js'
 su - magento -c '/var/www/html/magento2/bin/magento -vvv setup:static-content:deploy'
-
-
 
 # chown -R magento:www-data /var/www/html/
 
