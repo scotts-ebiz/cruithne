0d44e003fc30f57c444b49d76f244415d048c4de
Travis <Travis.Casper@Scotts.com>
2019-03-06 15:41:08 -0500

    Put the neelam special before the static-deploy (#190)

--- build/ondemand-entrypoint.sh
+++ build/ondemand-entrypoint.sh
@@ -29,11 +29,10 @@ su - magento -c 'php /var/www/html/magento2/bin/magento setup:config:set --sessi
 # su - magento -c 'gulp clean -f /var/www/html/magento2/tools/gulpfile.js'
 # su - magento -c 'gulp styles -f /var/www/html/magento2/tools/gulpfile.js'
 
-
-su - magento -c '/var/www/html/magento2/bin/magento setup:static-content:deploy'
-
 # This is a Patch for jquery.mobile to allow mouse events
 su - magento -c 'sed -i "s/.*mouseEventProps = .*/\tmouseEventProps = 0,/" /var/www/html/magento2/pub/static/frontend/Scottsbase/mygro/en_US/jquery/jquery.mobile.custom.js'
+
+su - magento -c '/var/www/html/magento2/bin/magento setup:static-content:deploy'
 
 su - magento -c '/var/www/html/magento2/bin/magento -v cache:flush'
 
