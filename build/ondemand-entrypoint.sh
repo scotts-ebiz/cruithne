61893911b5a145c131a43c0f3c54f035811fd43c
Travis <Travis.Casper@Scotts.com>
2019-03-05 19:11:01 -0500

    Getting Deploy to work immediately (#182)
    * just a test push to see if this triggers a build
    
    * minor change to get the trigger to work
    
    * minor change to get apache to run at deploy
    
    * trying to get to execute in the entrypoint
    
    * not sure what did it so I'm commeting out the entrypoint code
    
    * tried a different way to execute it
    
    * removed the CMD from dockerfile
    
    * minor update and some comments in the entrypoint

--- build/ondemand-entrypoint.sh
+++ build/ondemand-entrypoint.sh
@@ -16,7 +16,7 @@ COMMAND="$@"
 
 # rm -rf /var/www/html/magento2/var/generation/*
 # su - magento -c '/var/www/html/magento2/bin/magento deploy:mode:set -s developer'
-# su - magento -c '/var/www/html/magento2/bin/magento deploy:mode:set -s production'
+su - magento -c '/var/www/html/magento2/bin/magento deploy:mode:set -s production'
 # su - magento -c '/var/www/html/magento2/bin/magento setup:upgrade'
 # su - magento -c '/var/www/html/magento2/bin/magento setup:di:compile'
 
@@ -25,11 +25,15 @@ su - magento -c 'php /var/www/html/magento2/bin/magento setup:config:set --cache
 su - magento -c 'php /var/www/html/magento2/bin/magento setup:config:set --page-cache=redis --page-cache-redis-server=10.0.2.3 --page-cache-redis-db=1'
 su - magento -c 'php /var/www/html/magento2/bin/magento setup:config:set --session-save=redis --session-save-redis-host=10.0.2.3 --session-save-redis-log-level=3 --session-save-redis-db=2 -q'
 
+# TODO these aren't working. says gulp isn't there not sure why it's not installed globally
 # su - magento -c 'gulp clean -f /var/www/html/magento2/tools/gulpfile.js'
 # su - magento -c 'gulp styles -f /var/www/html/magento2/tools/gulpfile.js'
-su - magento -c '/var/www/html/magento2/bin/magento -vvv setup:static-content:deploy'
+
+
+su - magento -c '/var/www/html/magento2/bin/magento -v setup:static-content:deploy'
+su - magento -c '/var/www/html/magento2/bin/magento -v cache:flush'
 
 # chown -R magento:www-data /var/www/html/
 
 # CMD "exec /usr/sbin/apachectl -DFOREGROUND -k start"
-/usr/sbin/apachectl -DFOREGROUND 
+exec /usr/sbin/apachectl -DFOREGROUND
