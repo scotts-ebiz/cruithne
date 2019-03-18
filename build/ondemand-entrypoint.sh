8b196e8fe38b961aea4f14d763a5c40b1d3574c2
Travis <Travis.Casper@Scotts.com>
2019-03-18 16:00:35 -0500

    commented out the JS code and redis for now (#205)
    pushing this through as build

--- build/ondemand-entrypoint.sh
+++ build/ondemand-entrypoint.sh
@@ -21,9 +21,10 @@ su - magento -c '/var/www/html/magento2/bin/magento deploy:mode:set -s productio
 # su - magento -c '/var/www/html/magento2/bin/magento setup:di:compile'
 
 # TODO We should abstract the server address....
-su - magento -c 'php /var/www/html/magento2/bin/magento setup:config:set --cache-backend=redis --cache-backend-redis-server=10.0.2.3   --cache-backend-redis-db=0'
-su - magento -c 'php /var/www/html/magento2/bin/magento setup:config:set --page-cache=redis --page-cache-redis-server=10.0.2.3 --page-cache-redis-db=1'
-su - magento -c 'php /var/www/html/magento2/bin/magento setup:config:set --session-save=redis --session-save-redis-host=10.0.2.3 --session-save-redis-log-level=3 --session-save-redis-db=2 -q'
+# Turning this off for now. 
+# su - magento -c 'php /var/www/html/magento2/bin/magento setup:config:set --cache-backend=redis --cache-backend-redis-server=10.0.2.3   --cache-backend-redis-db=0'
+# su - magento -c 'php /var/www/html/magento2/bin/magento setup:config:set --page-cache=redis --page-cache-redis-server=10.0.2.3 --page-cache-redis-db=1'
+# su - magento -c 'php /var/www/html/magento2/bin/magento setup:config:set --session-save=redis --session-save-redis-host=10.0.2.3 --session-save-redis-log-level=3 --session-save-redis-db=2 -q'
 
 # TODO these aren't working. says gulp isn't there not sure why it's not installed globally
 # su - magento -c 'gulp clean -f /var/www/html/magento2/tools/gulpfile.js'
@@ -33,7 +34,7 @@ su - magento -c 'php /var/www/html/magento2/bin/magento setup:config:set --sessi
 su - magento -c '/var/www/html/magento2/bin/magento setup:static-content:deploy'
 
 # This is a Patch for jquery.mobile to allow mouse events
-su - magento -c 'sed -i "s/.*mouseEventProps = .*/\tmouseEventProps = 0,/" /var/www/html/magento2/pub/static/frontend/Scottsbase/mygro/en_US/jquery/jquery.mobile.custom.js'
+# su - magento -c 'sed -i "s/.*mouseEventProps = .*/\tmouseEventProps = 0,/" /var/www/html/magento2/pub/static/frontend/Scottsbase/mygro/en_US/jquery/jquery.mobile.custom.js' || 
 
 su - magento -c '/var/www/html/magento2/bin/magento -v cache:flush'
 
