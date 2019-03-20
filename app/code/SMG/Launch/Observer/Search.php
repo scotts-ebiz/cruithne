7c52243e054af139b9d802665a0378ddb8382659
saad-scotts <saad.yusuf@scotts.com>
2019-03-20 14:34:06 -0500

    Adobe Launch Extension

---
+++ app/code/SMG/Launch/Observer/Search.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace SMG\Launch\Observer;
+
+use Magento\Framework\Event\ObserverInterface;
+
+class Search implements ObserverInterface {
+
+	/** @var \SMG\Launch\Model\Session $_launchSession */
+	protected $_launchSession;
+	/** @var \Magento\Checkout\Model\Session $_checkoutSession */
+	protected $_checkoutSession;
+	/** @var  \SMG\Launch\Helper\Data $_launchHelper */
+	protected $_launchHelper;
+	/** @var \Magento\Search\Helper\Data $_searchHelper */
+	protected $_searchHelper;
+
+	public function __construct(
+		\SMG\Launch\Model\Session $fbPixelSession,
+		\Magento\Checkout\Model\Session $checkoutSession,
+		\SMG\Launch\Helper\Data $helper,
+		\Magento\Search\Helper\Data $searchHelper
+	) {
+		$this->_launchSession = $fbPixelSession;
+		$this->_checkoutSession = $checkoutSession;
+		$this->_launchHelper = $helper;
+		$this->_searchHelper = $searchHelper;
+	}
+
+	/**
+	 * @param \Magento\Framework\Event\Observer $observer
+	 *
+	 * @return void
+	 */
+	public function execute( \Magento\Framework\Event\Observer $observer ) {
+		$text = $this->_searchHelper->getEscapedQueryText();
+
+
+
+		$data = [
+			'search_string' => $text
+		];
+
+		$this->_launchSession->setSearch($data);
+
+		return $this;
+	}
+}
---
+++ app/code/SMG/Launch/Observer/WishlistAddProduct.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace SMG\Launch\Observer;
+
+use Magento\Framework\Event\ObserverInterface;
+
+class WishlistAddProduct implements ObserverInterface {
+
+	/** @var \SMG\Launch\Model\Session $_fbPixelSession */
+	protected $_fbPixelSession;
+	/** @var \Magento\Checkout\Model\Session $_checkoutSession */
+	protected $_checkoutSession;
+	/** @var  \SMG\Launch\Helper\Data $_fbPixelHelper */
+	protected $_fbPixelHelper;
+
+	public function __construct(
+		\SMG\Launch\Model\Session $fbPixelSession,
+		\Magento\Checkout\Model\Session $checkoutSession,
+		\SMG\Launch\Helper\Data $helper
+	) {
+		$this->_fbPixelSession = $fbPixelSession;
+		$this->_checkoutSession = $checkoutSession;
+		$this->_fbPixelHelper         = $helper;
+	}
+
+	/**
+	 * @param \Magento\Framework\Event\Observer $observer
+	 *
+	 * @return void
+	 */
+	public function execute( \Magento\Framework\Event\Observer $observer ) {
+		/** @var \Magento\Catalog\Model\Product $product */
+		$product = $observer->getProduct();
+		if (!$this->_fbPixelHelper->isAddToWishlistPixelEnabled() || !$product) {
+			return $this;
+		}
+
+		$data = [
+			'content_type' => 'product',
+			'content_ids' => [$product->getSku()],
+			'value' => $product->getFinalPrice(),
+			'currency' => $this->_fbPixelHelper->getCurrencyCode()
+		];
+
+		$this->_fbPixelSession->setAddToWishlist($data);
+
+		return $this;
+	}
+}
