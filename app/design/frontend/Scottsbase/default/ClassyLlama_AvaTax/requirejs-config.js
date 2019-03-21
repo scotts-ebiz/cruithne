02bfd87863f81f21e668e9ff6fe98da0a91468fe
Dzmitry Masiul <dmitry.masiul@classyllama.com>
2019-03-21 23:37:53 +0300

    SMG-610_36: - requirejs adjusted

--- app/design/frontend/Scottsbase/default/ClassyLlama_AvaTax/requirejs-config.js
+++ app/design/frontend/Scottsbase/default/ClassyLlama_AvaTax/requirejs-config.js
@@ -1,14 +1,5 @@
 
 var config = {
-    map: {
-        '*': {
-            "Magento_Checkout/js/model/shipping-save-processor/gift-registry": 'ClassyLlama_AvaTax/js/model/shipping-save-processor/gift-registry',
-            "Magento_Tax/template/checkout/cart/totals/tax": 'ClassyLlama_AvaTax/template/checkout/cart/totals/tax',
-            "Magento_Tax/template/checkout/summary/tax": 'ClassyLlama_AvaTax/template/checkout/summary/tax',
-            // Add the following alias to provide compatibility with Magento 2.2
-            addressValidation: 'ClassyLlama_AvaTax/js/addressValidation'
-        }
-    },
     config: {
         mixins: {
             'Magento_Checkout/js/view/payment/default': {
