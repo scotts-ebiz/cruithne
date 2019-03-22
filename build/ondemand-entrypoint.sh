4076f5a368923d708f30fa5f3e2ea75a3503b1d9
Travis Casper <Travis.Casper@scotts.com>
2019-03-22 14:25:16 -0400

    did some permissions and removed some temporary tweaks that were related to cart

--- build/ondemand-entrypoint.sh
+++ build/ondemand-entrypoint.sh
@@ -33,12 +33,9 @@ su - magento -c '/var/www/html/magento2/bin/magento deploy:mode:set -s productio
 
 su - magento -c '/var/www/html/magento2/bin/magento setup:static-content:deploy'
 
-# This is a Patch for jquery.mobile to allow mouse events
-# su - magento -c 'sed -i "s/.*mouseEventProps = .*/\tmouseEventProps = 0,/" /var/www/html/magento2/pub/static/frontend/Scottsbase/mygro/en_US/jquery/jquery.mobile.custom.js' || 
-
 su - magento -c '/var/www/html/magento2/bin/magento -v cache:flush'
 
-# chown -R magento:www-data /var/www/html/
+chown -R magento:www-data /var/www/html/
 
 # For the readiness check
 touch /tmp/healthy
