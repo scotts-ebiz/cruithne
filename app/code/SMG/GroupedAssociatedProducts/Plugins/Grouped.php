4a791cdc7c30afc5f2d542f3b1595b7f3dd04ef9
Ian Gall <ian.gall@scotts.com>
2019-04-17 17:35:54 -0400

    Added files for plugin for grouped php

---
+++ app/code/SMG/GroupedAssociatedProducts/Plugins/Grouped.php
@@ -0,0 +1,12 @@
+<?php
+/** Experiment on a Plugin for Grouped.php **/
+
+namespace SMG\GroupedAssociatedProducts\Plugins;
+
+class Grouped {
+  public function aftergetAssociatedProducts(){
+    return 'Test Test';
+  }
+}
+
+
