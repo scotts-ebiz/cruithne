39e5ed29992b5ba2d935c74965c88a026f148389
Travis Casper <Travis.Casper@scotts.com>
2019-04-22 13:57:10 -0400

    added an SMTP extension that should allow us to route emails correctly

--- app/etc/config.php
+++ app/etc/config.php
@@ -268,6 +268,8 @@ return [
         'Magento_ProductVideoStaging' => 1,
         'Magento_WishlistAnalytics' => 1,
         'Magento_Worldpay' => 1,
+        'Mageplaza_Core' => 1,
+        'Mageplaza_Smtp' => 1,
         'SMG_Api' => 1,
         'SMG_CreditReason' => 1,
         'SMG_Creditvantiv' => 1,
