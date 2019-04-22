a708c3eaf8bc144d29e0cecb2cd7afb19ae8d502
Ian Gall <ian.gall@scotts.com>
2019-04-22 12:11:46 -0400

    Adding more logic to utilize the resource model

--- app/code/SMG/GroupedAssociatedProducts/Plugin/Grouped.php
+++ app/code/SMG/GroupedAssociatedProducts/Plugin/Grouped.php
@@ -1,5 +1,4 @@
 <?php
-/** Experiment on a Plugin for Grouped.php **/
 
 namespace SMG\GroupedAssociatedProducts\Plugin;
 
@@ -8,78 +7,43 @@ class Grouped extends \Magento\GroupedProduct\Block\Product\View\Type\Grouped {
 
   protected $_productsFactory;
 
+  protected $_productResource;
 
-  //Create a Factory Object to handle the array for $result this must be instantiated in a construct
-  public function __construct(\SMG\GroupedAssociatedProducts\Model\ProductsFactory $productsFactory){
-    $this->_productsFactory = $productsFactory;
+
+  /* Use Constructor to:
+   * Create a Factory Object to handle the array for $result this must be instantiated in a construct
+   * Create a ResourceModel for Products
+  */
+
+  public function __construct(\SMG\GroupedAssociatedProducts\Model\ProductsFactory $productsFactory,
+  \Magneto\GroupedProduct\Model\ResourceModel\Product $productsResource
+  ){
+      $this->_productsFactory = $productsFactory;
+
+      $this->_productResource = $productsResource;
   }
 
 
-
-  // Q - Do I need to load $results into function?
   public function afterGetAssociatedProducts($result) {
 
-
-      //Create instance of $_productsFactory factory object and call it $products and put $results into it
-      // Q - Do I need to load $results into factory object, if dont what data can it play with?
       $products = $this->_productsFactory->create();
 
-
-      /*
-      1. Do I get entity_id? Why? From where? Do I use load(). How do $results have short_description and yet
-      I need to assign short_description to getAssociatedProduct $results?
-
-      2. I need short_description how to I get that, from where?
-
-      3. I need to load short_description in the results of associatedProducts, how do I do that?
-      foreach?
-
-      */
+      foreach ($result as $associatedProductItem) {
+          $productId = $result->getData('entity_id');
 
 
-      //Get entity_id from $products factory object
-      $productId = $products->getData('entity_id');
+          //Use the productResource to select the $productId and place it in $products Factory object
+          $this->_productResource->load($products, $productId);
 
 
-      //Get short_description from $products factory object
-      $shortDescription = $products->getData('short_description');
+          //In the factory object select the valye for 'short_description' and set it to $shortDescription
+          $shortDescription = $products->getData('short_description');
 
 
-      $productResourceModel->load($products, $productId);
-
-
-
-
-
-
-
+          //For each $associatedProductItem take the value of $shortDescription and set it to 'short_description'
+          $associatedProductItem->setData('short_description', $shortDescription);
+      }
 
     return $result;
   }
-
-
 }
-
-
-
-
-
-
-
-
-//foreach ($result as $item)
-//{
-
-// get the short description from product using $item->getId();
-// need Factory and ResourceModel
-// Create constructor
-// Instatiate vars
-
-//$product = $_productFactory->create();
-//$_productResource->load($product, $item->getId());
-
-//$shortDescription = $product->getData('short_description');
-
-// add short description
-//$item->setData('short_description', $shortDescription);
-//}
