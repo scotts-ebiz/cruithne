82d9ceeadf4a61f98c1ce8a02d17d86bdb373a0b
Ian Gall <ian.gall@scotts.com>
2019-04-24 12:49:45 -0400

    Still made changes to make short descript work

--- app/code/SMG/GroupedAssociatedProducts/Plugin/Grouped.php
+++ app/code/SMG/GroupedAssociatedProducts/Plugin/Grouped.php
@@ -2,41 +2,54 @@
 
 namespace SMG\GroupedAssociatedProducts\Plugin;
 
+use Psr\Log\LoggerInterface;
+
 class Grouped extends \Magento\GroupedProduct\Block\Product\View\Type\Grouped {
 
+  protected $_logger;
 
   protected $_productsFactory;
 
-  protected $_productResource;
+  protected $_productsResource;
 
 
-  /* Use Constructor to:
-   * Create a Factory Object to handle the array for $result this must be instantiated in a construct
+  /* Use Construct to:
+   * Create a Factory Object for ProductFactory
    * Create a ResourceModel for Products
+   * Create logger var to utilize for debugging as need be
   */
 
-  public function __construct(\SMG\GroupedAssociatedProducts\Model\ProductsFactory $productsFactory,
-  \Magneto\GroupedProduct\Model\ResourceModel\Product $productsResource
+  public function __construct(\Psr\Log\LoggerInterface $logger,
+                              \Magento\Catalog\Model\ProductFactory $productsFactory,
+  \Magento\Catalog\Model\ResourceModel\Product $productsResource
   ){
+      $this->_logger = $logger;
       $this->_productsFactory = $productsFactory;
-
-      $this->_productResource = $productsResource;
+      $this->_productsResource = $productsResource;
   }
 
 
-  public function afterGetAssociatedProducts($result) {
+  public function afterGetAssociatedProducts(\Magento\GroupedProduct\Block\Product\View\Type\Grouped $subject,
+                                             $result) {
 
       $products = $this->_productsFactory->create();
 
+      //Loop through $results
       foreach ($result as $associatedProductItem) {
-          $productId = $result->getData('entity_id');
 
 
-          //Use the productResource to select the $productId and place it in $products Factory object
-          $this->_productResource->load($products, $productId);
+          //Get entity_id
+          $productId = $associatedProductItem->getData('entity_id');
 
 
-          //In the factory object select the valye for 'short_description' and set it to $shortDescription
+          /*
+            Use the resource model _productResource to load all data for specific $productId and place in factory
+            object $products
+          */
+          $this->_productsResource->load($products, $productId);
+
+
+          //In the factory object $product select the value for 'short_description' and set it to $shortDescription
           $shortDescription = $products->getData('short_description');
 
 
