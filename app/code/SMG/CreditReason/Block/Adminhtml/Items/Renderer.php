ca4da4050400af47672a7b932c3aff4fb01ae6f3
hoose145 <hoose145@gmail.com>
2019-05-15 15:02:16 -0400

    ECOM-598: Fixed issue with Credit Memo Saving.  Updated to be a Plugin.  Updated code to handle bundles.  Updated Vagrant File to use the nfs by default. (#318)

--- app/code/SMG/CreditReason/Block/Adminhtml/Items/Renderer.php
+++ app/code/SMG/CreditReason/Block/Adminhtml/Items/Renderer.php
@@ -1,10 +1,17 @@
 <?php
+/**
+ * Created by PhpStorm.
+ * User: nvanhoose
+ * Date: 5/14/19
+ * Time: 9:01 AM
+ */
 
-namespace SMG\CreditReason\Block\Adminhtml\Items\Renderer;
+namespace SMG\CreditReason\Block\Adminhtml\Items;
 
+use Magento\Framework\Serialize\Serializer\Json;
 use SMG\CreditReason\Helper\CreditReasonHelper;
 
-class DefaultRenderer extends \Magento\Sales\Block\Adminhtml\Items\Renderer\DefaultRenderer
+class Renderer extends \Magento\Bundle\Block\Adminhtml\Sales\Order\Items\Renderer
 {
     /**
      * @var CreditReasonHelper
@@ -12,23 +19,24 @@ class DefaultRenderer extends \Magento\Sales\Block\Adminhtml\Items\Renderer\Defa
     protected $_creditReasonHelper;
 
     /**
-     * DefaultRenderer constructor.
-     *
+     * Renderer constructor.
      * @param \Magento\Backend\Block\Template\Context $context
      * @param \Magento\CatalogInventory\Api\StockRegistryInterface $stockRegistry
      * @param \Magento\CatalogInventory\Api\StockConfigurationInterface $stockConfiguration
      * @param \Magento\Framework\Registry $registry
      * @param CreditReasonHelper $creditReasonHelper
      * @param array $data
+     * @param Json|null $serializer
      */
     public function __construct(\Magento\Backend\Block\Template\Context $context,
         \Magento\CatalogInventory\Api\StockRegistryInterface $stockRegistry,
         \Magento\CatalogInventory\Api\StockConfigurationInterface $stockConfiguration,
         \Magento\Framework\Registry $registry,
         CreditReasonHelper $creditReasonHelper,
-        array $data = [])
+        array $data = [],
+        Json $serializer = null)
     {
-        parent::__construct($context, $stockRegistry, $stockConfiguration, $registry, $data);
+        parent::__construct($context, $stockRegistry, $stockConfiguration, $registry, $data, $serializer);
 
         $this->_creditReasonHelper = $creditReasonHelper;
     }
