3c31ce7cc3ce080d2d36180db029f66d8692c8a6
Travis Casper <Travis.Casper@scotts.com>
2019-05-21 09:29:53 -0400

    removed all the build commands again so that I can get this working

--- build/ondemand-entrypoint.sh
+++ build/ondemand-entrypoint.sh
@@ -21,22 +21,22 @@ COMMAND="$@"
 #service collector start
 
 # su - magento -c '/var/www/html/magento2/bin/magento deploy:mode:set -s developer'
-su - magento -c '/var/www/html/magento2/bin/magento deploy:mode:set -s production'
-su - magento -c '/var/www/html/magento2/bin/magento setup:install'
-su - magento -c '/var/www/html/magento2/bin/magento setup:upgrade'
-su - magento -c '/var/www/html/magento2/bin/magento setup:di:compile'
+# su - magento -c '/var/www/html/magento2/bin/magento deploy:mode:set -s production'
+# su - magento -c '/var/www/html/magento2/bin/magento setup:install'
+# su - magento -c '/var/www/html/magento2/bin/magento setup:upgrade'
+# su - magento -c '/var/www/html/magento2/bin/magento setup:di:compile'
 
 # TODO We should abstract the server address....
 # su - magento -c 'php /var/www/html/magento2/bin/magento setup:config:set --cache-backend=redis --cache-backend-redis-server=10.0.2.3   --cache-backend-redis-db=0 -q'
 # su - magento -c 'php /var/www/html/magento2/bin/magento setup:config:set --page-cache=redis --page-cache-redis-server=10.0.2.3 --page-cache-redis-db=1 -q'
 # su - magento -c 'php /var/www/html/magento2/bin/magento setup:config:set --session-save=redis --session-save-redis-host=10.0.2.3 --session-save-redis-log-level=3 --session-save-redis-db=2 -q'
 
-su - magento -c 'gulp clean -f /var/www/html/magento2/tools/gulpfile.js'
-su - magento -c 'cd /var/www/html/magento2/tools && npm rebuild node-sass && gulp styles -f /var/www/html/magento2/tools/gulpfile.js'
+# su - magento -c 'gulp clean -f /var/www/html/magento2/tools/gulpfile.js'
+# su - magento -c 'cd /var/www/html/magento2/tools && npm rebuild node-sass && gulp styles -f /var/www/html/magento2/tools/gulpfile.js'
 
-su - magento -c '/var/www/html/magento2/bin/magento setup:static-content:deploy'
-su - magento -c '/var/www/html/magento2/bin/magento -v index:reindex'
-su - magento -c '/var/www/html/magento2/bin/magento -v cache:flush'
+# su - magento -c '/var/www/html/magento2/bin/magento setup:static-content:deploy'
+# su - magento -c '/var/www/html/magento2/bin/magento -v index:reindex'
+# su - magento -c '/var/www/html/magento2/bin/magento -v cache:flush'
 
 # For the readiness check
 touch /tmp/healthy
