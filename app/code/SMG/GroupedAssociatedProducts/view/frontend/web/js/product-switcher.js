500b049ceed99bb9ec8dc428e5a17b5219370c5f
Ian Gall <ian.gall@scotts.com>
2019-05-24 16:24:05 -0400

    Adding more js logic

--- app/code/SMG/GroupedAssociatedProducts/view/frontend/web/js/product-switcher.js
+++ app/code/SMG/GroupedAssociatedProducts/view/frontend/web/js/product-switcher.js
@@ -1,3 +1,7 @@
 define(['jquery'], function($){
-
+    $('#product_selector').change(function () {
+        var selectValue = this.value;
+        //alert( this.value );
+        alert(selectValue);
+    });
 });
