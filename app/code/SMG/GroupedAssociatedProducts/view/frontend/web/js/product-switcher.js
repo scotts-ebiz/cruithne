122486ea0a2bedbee3e0c13b7ee92008ddb89269
Ian Gall <ian.gall@scotts.com>
2019-05-26 16:23:11 -0400

    Adding yet again new logic to deal with multiple products using inverse of has class

--- app/code/SMG/GroupedAssociatedProducts/view/frontend/web/js/product-switcher.js
+++ app/code/SMG/GroupedAssociatedProducts/view/frontend/web/js/product-switcher.js
@@ -1,7 +1,24 @@
 define(['jquery'], function($){
     $('#product_selector').change(function () {
         var selectValue = this.value;
-        //alert( this.value );
-        alert(selectValue);
+        //alert(selectValue);
+
+        if ($(selectValue == 'product_item_id__1') && !$("#product_summary_id__1").hasClass("active")){
+            $('#product_summary_id__1').addClass('active');
+            $('#product_summary_id__2').removeClass('active');
+            $('#product_summary_id__3').removeClass('active');
+            $('#product_summary_id__4').removeClass('active');
+            $('#product_summary_id__5').removeClass('active');
+            $('#product_summary_id__6').removeClass('active');
+        } else if ($(selectValue == 'product_item_id__2') && !$("#product_summary_id__2").hasClass("active")){
+            $('#product_summary_id__2').addClass('active');
+            $('#product_summary_id__1').removeClass('active');
+            $('#product_summary_id__3').removeClass('active');
+            $('#product_summary_id__4').removeClass('active');
+            $('#product_summary_id__5').removeClass('active');
+            $('#product_summary_id__6').removeClass('active');
+        } else {
+            alert("There's a third option!");
+        };
     });
 });
