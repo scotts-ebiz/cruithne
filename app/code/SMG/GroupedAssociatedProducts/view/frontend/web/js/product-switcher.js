56951ba1244ded308f18cdc048a1a3d93bde326e
Ian Gall <ian.gall@scotts.com>
2019-05-26 20:42:05 -0400

    Adding additional values for product items

--- app/code/SMG/GroupedAssociatedProducts/view/frontend/web/js/product-switcher.js
+++ app/code/SMG/GroupedAssociatedProducts/view/frontend/web/js/product-switcher.js
@@ -17,8 +17,36 @@ define(['jquery'], function($){
             $('#product_summary_id__4').removeClass('active');
             $('#product_summary_id__5').removeClass('active');
             $('#product_summary_id__6').removeClass('active');
+        } else if ($(selectValue == 'product_item_id__3') && !$("#product_summary_id__3").hasClass("active")){
+            $('#product_summary_id__3').addClass('active');
+            $('#product_summary_id__1').removeClass('active');
+            $('#product_summary_id__2').removeClass('active');
+            $('#product_summary_id__4').removeClass('active');
+            $('#product_summary_id__5').removeClass('active');
+            $('#product_summary_id__6').removeClass('active');
+        } else if ($(selectValue == 'product_item_id__4') && !$("#product_summary_id__4").hasClass("active")){
+            $('#product_summary_id__4').addClass('active');
+            $('#product_summary_id__1').removeClass('active');
+            $('#product_summary_id__2').removeClass('active');
+            $('#product_summary_id__3').removeClass('active');
+            $('#product_summary_id__5').removeClass('active');
+            $('#product_summary_id__6').removeClass('active');
+        } else if ($(selectValue == 'product_item_id__5') && !$("#product_summary_id__5").hasClass("active")){
+            $('#product_summary_id__5').addClass('active');
+            $('#product_summary_id__1').removeClass('active');
+            $('#product_summary_id__2').removeClass('active');
+            $('#product_summary_id__3').removeClass('active');
+            $('#product_summary_id__4').removeClass('active');
+            $('#product_summary_id__6').removeClass('active');
+        } else if ($(selectValue == 'product_item_id__6') && !$("#product_summary_id__6").hasClass("active")){
+            $('#product_summary_id__6').addClass('active');
+            $('#product_summary_id__1').removeClass('active');
+            $('#product_summary_id__2').removeClass('active');
+            $('#product_summary_id__3').removeClass('active');
+            $('#product_summary_id__4').removeClass('active');
+            $('#product_summary_id__5').removeClass('active');
         } else {
-            alert("There's a third option!");
-        };
+            //do nothing
+        }
     });
 });
