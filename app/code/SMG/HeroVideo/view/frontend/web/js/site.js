916400e94ecb331dc9a05543fd06de3f8256f9df
Ian Gall <ian.gall@scotts.com>
2019-06-05 16:37:17 -0400

    Adding reference to the site js file

---
+++ app/code/SMG/HeroVideo/view/frontend/web/js/site.js
@@ -0,0 +1,18 @@
+define([
+    'jquery'
+], function ($) {
+    alert('This is a test!');
+    /* Product Callout Video Widget: Start */
+    $(".video-link").on("click", function (e) {
+        var embedCode = $(this).find(".embed-code").html();
+        $("body").toggleClass("video-open");
+        $("#video-container .video-frame").html(embedCode);
+        e.preventDefault();
+    });
+
+    $(".video-overlay").on("click", function (e) {
+        $("body").toggleClass("video-open");
+        $("#video-container .video-frame").html('');
+    });
+    /* Product Callout Video Widget: End */
+});
