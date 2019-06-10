d0d34effab686cda81428d816ca2a16806d5bb5e
Ian Gall <ian.gall@scotts.com>
2019-06-10 17:30:43 -0400

    Adding changes to js phtml and xml file to fix video conflict

--- app/code/SMG/ProductCalloutVideo/view/frontend/web/js/site.js
+++ app/code/SMG/ProductCalloutVideo/view/frontend/web/js/site.js
@@ -3,16 +3,16 @@ define([
 ], function ($) {
     "use strict";
     /* Product Callout Video Widget: Start */
-    $(".video-link").on("click", function (e) {
+    $(".productCalloutVideo-video-link").on("click", function (e) {
         var embedCode = $(this).find(".embed-code").html();
         $("body").toggleClass("video-open");
-        $("#video-container .video-frame").html(embedCode);
+        $("#productCalloutVideo-video-container .video-frame").html(embedCode);
         e.preventDefault();
     });
 
-    $(".video-overlay").on("click", function (e) {
+    $(".productCalloutVideo-video-overlay").on("click", function (e) {
         $("body").toggleClass("video-open");
-        $("#video-container .video-frame").html('');
+        $("#productCalloutVideo-video-container .video-frame").html('');
     });
     /* Product Callout Video Widget: End */
 });
