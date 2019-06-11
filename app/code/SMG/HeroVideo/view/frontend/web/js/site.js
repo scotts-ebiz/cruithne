621d2a152e78e84fd051606c1adcf1fa7df6062d
Ian Gall <ian.gall@scotts.com>
2019-06-11 10:37:08 -0400

    Customized each video centric widget further for JS fix

--- app/code/SMG/HeroVideo/view/frontend/web/js/site.js
+++ app/code/SMG/HeroVideo/view/frontend/web/js/site.js
@@ -1,17 +1,18 @@
 define([
     'jquery'
 ], function ($) {
+    "use strict";
     /* Video Widget Code: Start */
-    $(".video-link").on("click", function (e) {
+    $(".heroVideo-video-link").on("click", function (e) {
         var embedCode = $(this).find(".embed-code").html();
-        $("body").toggleClass("video-open");
-        $("#video-container .video-frame").html(embedCode);
+        $("body").toggleClass("heroVideo-video-open");
+        $("#heroVideo-video-container .video-frame").html(embedCode);
         e.preventDefault();
     });
 
-    $(".video-overlay").on("click", function (e) {
-        $("body").toggleClass("video-open");
-        $("#video-container .video-frame").html('');
+    $(".heroVideo-video-overlay").on("click", function (e) {
+        $("body").toggleClass("heroVideo-video-open");
+        $("#heroVideo-video-container .video-frame").html('');
     });
     /* Video Widget Code: End */
 });
--- app/code/SMG/ProductCalloutVideo/view/frontend/web/js/site.js
+++ app/code/SMG/ProductCalloutVideo/view/frontend/web/js/site.js
@@ -5,13 +5,13 @@ define([
     /* Product Callout Video Widget: Start */
     $(".productCalloutVideo-video-link").on("click", function (e) {
         var embedCode = $(this).find(".embed-code").html();
-        $("body").toggleClass("video-open");
+        $("body").toggleClass("productCalloutVideo-video-open");
         $("#productCalloutVideo-video-container .video-frame").html(embedCode);
         e.preventDefault();
     });
 
     $(".productCalloutVideo-video-overlay").on("click", function (e) {
-        $("body").toggleClass("video-open");
+        $("body").toggleClass("productCalloutVideo-video-open");
         $("#productCalloutVideo-video-container .video-frame").html('');
     });
     /* Product Callout Video Widget: End */
