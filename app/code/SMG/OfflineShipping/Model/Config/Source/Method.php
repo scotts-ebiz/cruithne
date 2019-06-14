722663af9e83dc2ea0bbc9d73a5de9be5c4ba0a2
Stephen Biston <stephen.biston@classyllama.com>
2019-06-14 14:40:37 -0500

    SMG-610_42 - Revert unnecessary changes

--- app/code/SMG/OfflineShipping/Model/Config/Source/Method.php
+++ app/code/SMG/OfflineShipping/Model/Config/Source/Method.php
@@ -6,9 +6,6 @@
 namespace SMG\OfflineShipping\Model\Config\Source;
 
 use Magento\Framework\Data\OptionSourceInterface;
-use Magento\Framework\Exception\NoSuchEntityException;
-use Magento\Store\Model\StoreManagerInterface;
-use \SMG\OfflineShipping\Model\ResourceModel\ShippingConditionCode\CollectionFactory as ShippingConditionCollectionFactory;
 
 /**
  * Class Method
@@ -17,67 +14,26 @@ use \SMG\OfflineShipping\Model\ResourceModel\ShippingConditionCode\CollectionFac
 class Method implements OptionSourceInterface
 {
     /**
-     * @var \Magento\Framework\ObjectManager\ContextInterface
-     */
-    protected $context;
-
-    /**
-     * @var StoreManagerInterface
-     */
-    protected $storeManager;
-
-    /**
-     * @var ShippingConditionCollectionFactory
-     */
-    protected $shippingConditionCollectionFactory;
-
-    /**
-     * Method constructor.
-     *
-     * @param ShippingConditionCollectionFactory                $collectionFactory
-     * @param StoreManagerInterface                             $storeManager
-     * @param \Magento\Framework\ObjectManager\ContextInterface $context
-     */
-    public function __construct(
-        ShippingConditionCollectionFactory $collectionFactory,
-        StoreManagerInterface $storeManager,
-        \Magento\Framework\App\Action\Context $context
-    ) {
-        $this->shippingConditionCollectionFactory = $collectionFactory;
-        $this->storeManager = $storeManager;
-        $this->context = $context;
-    }
-
-    /**
      * @return array
      */
     public function toOptionArray()
     {
         $options = [];
-        /** @var \SMG\OfflineShipping\Model\ShippingConditionCode $method */
-        foreach ($this->getAvailableMethods($this->context->getRequest()->getParam('store')) as $method) {
-            $options[] = ['value' => $method->getShippingMethod(), 'label' => $method->getDescription()];
+        foreach ($this->getAvailableMethods() as $code => $title) {
+            $options[] = ['value' => $code, 'label' => $title];
         }
         return $options;
     }
 
     /**
-     * @param null|int|string $storeId
-     *
      * @return array
      */
-    public function getAvailableMethods($storeId = null)
+    public function getAvailableMethods()
     {
-        if ($storeId === null) {
-            try {
-                $storeId = $this->storeManager->getStore()->getId();
-            } catch (NoSuchEntityException $e) {
-                return [];
-            }
-        }
-        /** @var \SMG\OfflineShipping\Model\ResourceModel\ShippingConditionCode\Collection $collection */
-        $collection = $this->shippingConditionCollectionFactory->create();
-        $collection->addFieldToFilter('store_id', $storeId);
-        return $collection->getItems();
+        return [
+            'fedex-nextday' => __('Fedex Next Day'),
+            'fedex-2ndday' => __('Fedex 2nd Day'),
+            'flat-rate-shipping' => __('Flat Rate Shipping'),
+        ];
     }
 }
