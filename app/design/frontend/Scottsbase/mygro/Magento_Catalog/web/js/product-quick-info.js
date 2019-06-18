02b96c6466089b93acc0c389e18d540072980369
Ian Gall <ian.gall@scotts.com>
2019-06-18 15:13:36 -0400

    Included content divs on addtocart file and js changes and new img files

--- app/design/frontend/Scottsbase/mygro/Magento_Catalog/web/js/product-quick-info.js
+++ app/design/frontend/Scottsbase/mygro/Magento_Catalog/web/js/product-quick-info.js
@@ -1,3 +1,16 @@
 define(['jquery'], function($){
     alert('This is a testy!');
+
+    if ($("body").hasClass("product-sensors")){
+        alert('Product Sensors');
+        $('#sensors-quickInfo').addClass('active');
+        $('#controllers-quickInfo').removeClass('active');
+    } else if ($("body").hasClass("product-controllers")){
+        alert('Product Controllers');
+        $('#controllers-quickInfo').addClass('active');
+        $('#sensors-quickInfo').removeClass('active');
+    } else {
+        //Do nothing
+    }
+
 });
