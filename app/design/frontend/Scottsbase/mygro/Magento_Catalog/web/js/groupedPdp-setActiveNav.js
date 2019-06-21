810b05204197cd4ec4b13adf1eb4b631c9f81e38
Ian Gall <ian.gall@scotts.com>
2019-06-21 17:26:32 -0400

    Adding active nav logic to js

--- app/design/frontend/Scottsbase/mygro/Magento_Catalog/web/js/groupedPdp-setActiveNav.js
+++ app/design/frontend/Scottsbase/mygro/Magento_Catalog/web/js/groupedPdp-setActiveNav.js
@@ -1,4 +1,20 @@
 define(['jquery'], function($){
-    alert('This is the Active State JS');
 
+    if ($('body').hasClass('product-sensors')){
+        $('#mygro-sensors-page').addClass('active');
+        $('#mygro-controllers-page').removeClass('active');
+        $('#mygro-home-page').removeClass('active');
+
+    } else if ($('body').hasClass('product-controllers')) {
+        $('#mygro-controllers-page').addClass('active');
+        $('#mygro-sensors-page').removeClass('active');
+        $('#mygro-home-page').removeClass('active');
+
+    } else if ($('body').hasClass('cms-index-index')) {
+        $('#mygro-home-page').addClass('active');
+        $('#mygro-sensors-page').removeClass('active');
+        $('#mygro-controllers-page').removeClass('active');
+    } else {
+        //do nothing
+    }
 });
