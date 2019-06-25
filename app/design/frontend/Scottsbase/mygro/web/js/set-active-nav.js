e29d494bf4977b8b165cd1576c8a8a7e0b82970b
Ian Gall <ian.gall@scotts.com>
2019-06-25 11:02:26 -0400

    Still editing the js for active status

--- app/design/frontend/Scottsbase/mygro/web/js/set-active-nav.js
+++ app/design/frontend/Scottsbase/mygro/web/js/set-active-nav.js
@@ -1,5 +1,20 @@
 define(['jquery'], function($){
 
-    alert('Seth Avett rules!');
+       alert('Document is ready!');
+
+
+    if ($("body").hasClass("product-sensors")){
+        alert('Product Sensors Yo!');
+        $('#mygro-sensors-page').addClass('active');
+        $('#mygro-controllers-page').removeClass('active');
+        $('#mygro-home-page').removeClass('active');
+    } else if ($("body").hasClass("product-controllers")){
+        alert('Product Controllers yo!');
+        $('#mygro-controllers-page').addClass('active');
+        $('#mygro-sensors-page').removeClass('active');
+        $('#mygro-home-page').removeClass('active');
+    } else {
+        //Do nothing
+    }
 
 });
