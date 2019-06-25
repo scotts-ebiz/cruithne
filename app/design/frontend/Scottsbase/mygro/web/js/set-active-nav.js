b812f1ec0c541a5e41d702d37f95957a6282d2bc
Ian Gall <ian.gall@scotts.com>
2019-06-25 08:05:45 -0400

    Altered js for testing purposes

--- app/design/frontend/Scottsbase/mygro/web/js/set-active-nav.js
+++ app/design/frontend/Scottsbase/mygro/web/js/set-active-nav.js
@@ -2,21 +2,4 @@ define(['jquery'], function($){
 
     alert('Seth Avett rules!');
 
-    if ($('body').hasClass('product-sensors')){
-        $('#mygro-sensors-page').addClass('active');
-        $('#mygro-controllers-page').removeClass('active');
-        $('#mygro-home-page').removeClass('active');
-
-    } else if ($('body').hasClass('product-controllers')) {
-        $('#mygro-controllers-page').addClass('active');
-        $('#mygro-sensors-page').removeClass('active');
-        $('#mygro-home-page').removeClass('active');
-
-    } else if ($('body').hasClass('cms-index-index')) {
-        $('#mygro-home-page').addClass('active');
-        $('#mygro-sensors-page').removeClass('active');
-        $('#mygro-controllers-page').removeClass('active');
-    } else {
-        //do nothing
-    }
 });
