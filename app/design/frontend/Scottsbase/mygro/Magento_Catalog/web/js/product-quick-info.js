768fe9341a086c2cf6e8a61a1b560364cbae83b0
Ian Gall <ian.gall@scotts.com>
2019-06-26 09:42:55 -0400

    Remove js alert

--- app/design/frontend/Scottsbase/mygro/Magento_Catalog/web/js/product-quick-info.js
+++ app/design/frontend/Scottsbase/mygro/Magento_Catalog/web/js/product-quick-info.js
@@ -1,11 +1,9 @@
 define(['jquery'], function($){
 
     if ($("body").hasClass("product-sensors")){
-        alert('Product Sensors');
         $('#sensors-quickInfo').addClass('active');
         $('#controllers-quickInfo').removeClass('active');
     } else if ($("body").hasClass("product-controllers")){
-        alert('Product Controllers');
         $('#controllers-quickInfo').addClass('active');
         $('#sensors-quickInfo').removeClass('active');
     } else {
