1120567673c60a337c455da0a2796521453b4f82
Ian Gall <ian.gall@scotts.com>
2019-06-26 09:25:12 -0400

    Added the link to the home page in the correct manner

--- app/design/frontend/Scottsbase/mygro/web/js/set-active-nav.js
+++ app/design/frontend/Scottsbase/mygro/web/js/set-active-nav.js
@@ -1,19 +1,16 @@
 define(['jquery'], function($){
-
+//Set Active status for nav items
     if ($("body").hasClass("product-sensors")){
-        alert('Product Sensors Yo!');
         $('#mygro-sensors-page').addClass('active');
         $('#mygro-controllers-page').removeClass('active');
         $('#mygro-home-page').removeClass('active');
 
     } else if ($("body").hasClass("product-controllers")){
-        alert('Product Controllers yo!');
         $('#mygro-controllers-page').addClass('active');
         $('#mygro-sensors-page').removeClass('active');
         $('#mygro-home-page').removeClass('active');
 
     } else if ($("body").hasClass("cms-mygrohome")){
-        alert('You are home!');
         $('#mygro-home-page').addClass('active');
         $('#mygro-sensors-page').removeClass('active');
         $('#mygro-controllers-page').removeClass('active');
