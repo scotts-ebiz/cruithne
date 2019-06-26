e15bf3b5ee17abf016554ddc5a5ef4dd57470c2b
Ian Gall <ian.gall@scotts.com>
2019-06-25 22:21:19 -0400

    Making changes to nav

--- app/design/frontend/Scottsbase/mygro/web/js/set-active-nav.js
+++ app/design/frontend/Scottsbase/mygro/web/js/set-active-nav.js
@@ -1,18 +1,23 @@
 define(['jquery'], function($){
 
-       alert('Document is ready!');
-
-
     if ($("body").hasClass("product-sensors")){
         alert('Product Sensors Yo!');
         $('#mygro-sensors-page').addClass('active');
         $('#mygro-controllers-page').removeClass('active');
         $('#mygro-home-page').removeClass('active');
+
     } else if ($("body").hasClass("product-controllers")){
         alert('Product Controllers yo!');
         $('#mygro-controllers-page').addClass('active');
         $('#mygro-sensors-page').removeClass('active');
         $('#mygro-home-page').removeClass('active');
+
+    } else if ($("body").hasClass("cms-mygrohome")){
+        alert('You are home!');
+        $('#mygro-home-page').addClass('active');
+        $('#mygro-sensors-page').removeClass('active');
+        $('#mygro-controllers-page').removeClass('active');
+
     } else {
         //Do nothing
     }
