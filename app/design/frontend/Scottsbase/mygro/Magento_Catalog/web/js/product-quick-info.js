616fae670d7c6d158c8e5a9c474dd5af80b43ae4
Ian Gall <ian.gall@scotts.com>
2019-06-27 10:21:42 -0400

    Removed test alerts from js

--- app/design/frontend/Scottsbase/mygro/Magento_Catalog/web/js/product-quick-info.js
+++ app/design/frontend/Scottsbase/mygro/Magento_Catalog/web/js/product-quick-info.js
@@ -1,12 +1,9 @@
 define(['jquery'], function($){
-    alert('This is a testy!');
 
     if ($("body").hasClass("product-sensors")){
-        alert('Product Sensors');
         $('#sensors-quickInfo').addClass('active');
         $('#controllers-quickInfo').removeClass('active');
     } else if ($("body").hasClass("product-controllers")){
-        alert('Product Controllers');
         $('#controllers-quickInfo').addClass('active');
         $('#sensors-quickInfo').removeClass('active');
     } else {
