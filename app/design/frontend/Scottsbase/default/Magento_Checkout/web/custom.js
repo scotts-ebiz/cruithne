ac2ac8b4bc246c72036a07c6ff8d0355019940b4
saad-scotts <47223514+saad-scotts@users.noreply.github.com>
2019-07-14 14:06:29 +0530

    Update custom.js

--- app/design/frontend/Scottsbase/default/Magento_Checkout/web/custom.js
+++ app/design/frontend/Scottsbase/default/Magento_Checkout/web/custom.js
@@ -11,6 +11,7 @@ function($){
 		setTimeout(function(){
 			
 			$(".street > .control").append("<a class='cust-btn-add' href='javascript:void(0)'>Add New Line</a>");
+			
 			$("input[name='postcode']").attr('pattern', '[0-9]*');
 			$("input[name='postcode']").attr('inputmode', 'numeric');
 			$("input[name='telephone']").attr('pattern', '[0-9]*');
@@ -21,16 +22,8 @@ function($){
 				count += 1;
 				if (count == 1) {
 					$("div[name='shippingAddress.street.1']").css('display','block');
-					$("div[name='billingAddressauthorizenet_directpost.street.1']").css('display','block');
-					$("div[name='billingAddresscheckmo.street.1']").css('display','block');
-					$("div[name='billingAddressvantiv_cc.street.1']").css('display','block');
-					$("div[name='billingAddresscashondelivery.street.1']").css('display','block');
 				}else if(count == 2){
 					$("div[name='shippingAddress.street.2']").css('display','block');
-					$("div[name='billingAddressauthorizenet_directpost.street.2']").css('display','block');
-					$("div[name='billingAddresscheckmo.street.2']").css('display','block');
-					$("div[name='billingAddressvantiv_cc.street.2']").css('display','block');
-					$("div[name='billingAddresscashondelivery.street.2']").css('display','block');
 					$('.cust-btn-add').css({"pointer-events": "none", "color": "#c2c2c2"});
 				}
 				else{ 
