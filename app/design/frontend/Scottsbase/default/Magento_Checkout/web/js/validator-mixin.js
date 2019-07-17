b34f197d24355415af98e352a74680ca4c895efa
saad-scotts <saad.yusuf@scotts.com>
2019-07-17 14:51:54 -0500

    bug fixes

--- app/design/frontend/Scottsbase/default/Magento_Checkout/web/js/validator-mixin.js
+++ app/design/frontend/Scottsbase/default/Magento_Checkout/web/js/validator-mixin.js
@@ -25,7 +25,26 @@ define([
 					return !$.mage.isEmpty(value);
 				}
 			},
-			$.mage.__('Invalid Name')
+			$.mage.__('Please enter a valid name.')
+		);
+		validator.addRule(
+			'required-entry-bfirstname',
+			function (value) {
+				if ($(".billing-address-form input[name='firstname']").val() != '') {
+					if (!isNaN(value)) {
+						return false;
+					}
+					if( value.match( /^[a-zA-Z ]*$/) ) {
+						 return true
+					}else{
+						return false;
+					}
+					
+				} else {
+					return !$.mage.isEmpty(value);
+				}
+			},
+			$.mage.__('Please enter a valid name.')
 		);
 		validator.addRule(
 			'required-entry-lastname',
@@ -43,7 +62,25 @@ define([
 					return !$.mage.isEmpty(value);
 				}
 			},
-			$.mage.__('Invalid Name')
+			$.mage.__('Please enter a valid name.')
+		);
+		validator.addRule(
+			'required-entry-blastname',
+			function (value) {
+				if ($(".billing-address-form input[name='lastname']").val() != '') {
+					if (!isNaN(value)) {
+						return false;
+					}
+					if( value.match( /^[a-zA-Z ]*$/) ) {
+						 return true
+					}else{
+						return false;
+					}
+				} else {
+					return !$.mage.isEmpty(value);
+				}
+			},
+			$.mage.__('Please enter a valid name.')
 		);
 		validator.addRule(
 			'required-entry-pcode',
@@ -61,7 +98,19 @@ define([
 					return !$.mage.isEmpty(value);
 				}
 			},
-			$.mage.__('Invalid Zip Code')
+			$.mage.__('Please enter a valid zip code.')
+		);
+		validator.addRule(
+			'required-entry-bpcode',
+			function (value) {
+				if ($(".billing-address-form input[name='postcode']").val() != '') {
+					if(value.length < 5){
+						return false;
+					}
+					return true;
+				} 
+			},
+			$.mage.__('Please enter a valid zip code.')
 		);
 		validator.addRule(
 			'required-entry-telephone',
@@ -82,11 +131,36 @@ define([
 					}
 					
 
+				} else { 
+					return !$.mage.isEmpty(value);
+				}
+			},
+			$.mage.__('Please enter a valid phone.')
+		);
+		validator.addRule(
+			'required-entry-btelephone',
+			function (value) {
+				if ($(".billing-address-form input[name='telephone']").val() != '') {
+					var str = $(".billing-address-form input[name='telephone']").val();
+					if(str){
+						var mob = str.replace(/\-/g, '');
+						if(mob.length < 10){
+							return false;
+						}
+					}
+					var filter = /^((\+[1-9]{1,4}[ \-]*)|(\([0-9]{2,3}\)[ \-]*)|([0-9]{2,4})[ \-]*)*?[0-9]{3,4}?[ \-]*[0-9]{3,4}?$/;
+					if (value.length > 9 && filter.test(value)) {
+						return true;
+					}else {
+						return false;
+					}
+					
+
 				} else {
 					return !$.mage.isEmpty(value);
 				}
 			},
-			$.mage.__('Invalid Phone')
+			$.mage.__('Please enter a valid phone.')
 		);
 		
 		return validator;
