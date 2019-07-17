62dce0caa134fc3208f57e93f5c63077c6fabffc
Travis Casper <Travis.Casper@scotts.com>
2019-07-17 15:34:26 -0400

    updated with latest cluster information

--- environment/scripts/kubernetes-stage.sh
+++ environment/scripts/kubernetes-stage.sh
@@ -1,3 +1,3 @@
-gcloud container clusters get-credentials magento-stage-cluster --zone us-east1-b
+gcloud container clusters get-credentials magento-stage-ha-cluster --zone us-east1
 instance=$(kubectl get pod -l app=magento-stage -o jsonpath="{.items[0].metadata.name}")
 kubectl exec -it $instance -c stage-magento /bin/bash
