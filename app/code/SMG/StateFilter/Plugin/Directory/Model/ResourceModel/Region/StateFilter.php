08f7c82d7b5e8a28c2898eb9df4be2afbcc01419
nneemkar <neelam.neemkar@scotts.com>
2019-08-02 17:53:53 +0000

    Removed some of the states from dropdown

---
+++ app/code/SMG/StateFilter/Plugin/Directory/Model/ResourceModel/Region/StateFilter.php
@@ -0,0 +1,37 @@
+<?php
+namespace SMG\StateFilter\Plugin\Directory\Model\ResourceModel\Region;
+
+
+class StateFilter
+{
+   protected $disallowed = [
+    'Alaska',
+    'American Samoa',
+    'Armed Forces Africa',
+    'Armed Forces Americas',
+    'Armed Forces Canada',
+    'Armed Forces Europe',
+    'Armed Forces Middle East',
+    'Armed Forces Pacific',
+    'District of Columbia',
+    'Federated States Of Micronesia',
+    'Guam',
+    'Hawaii',
+    'Marshall Islands',
+    'Northern Mariana Islands',
+    'Palau',
+    'Puerto Rico',
+    'Virgin Islands'
+];
+
+    public function afterToOptionArray(\Magento\Directory\Model\ResourceModel\Region\Collection $subject, $options)
+    {
+        $result = array_filter($options, function ($option){
+            if(isset($option['label']))
+                return !in_array($option['label'], $this->disallowed);
+            return true;
+        });
+
+        return $result;
+    }
+}
