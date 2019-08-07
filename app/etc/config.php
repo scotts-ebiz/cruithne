c504572dc5f9bfb2acde261e3618f22bf14610fc
hoose145 <hoose145@gmail.com>
2019-08-07 09:06:02 -0400

    Update config.php

--- app/etc/config.php
+++ app/etc/config.php
@@ -284,7 +284,7 @@ return [
         'SMG_HeroVideoBtnWidget' => 1,
         'SMG_HeroZip' => 1,
         'SMG_IconList' => 1,
-        'SMG_ImageGallery' => 0,
+        'SMG_ImageGallery' => 0, //PLEASE MAKE SURE TO LEAVE THIS DISABLED IN GITHUB UNTIL 2.3.3
         'SMG_LandingPage' => 1,
         'SMG_Launch' => 1,
         'SMG_OfflineShipping' => 1,
