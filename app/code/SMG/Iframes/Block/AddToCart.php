8bfb8019495ea89c4fd21b5cb61b5bfed2eb5974
Beth Pauley <elizabeth.pauley@scotts.com>
2019-08-26 15:27:47 -0400

    GOM1-63: Improvements per comments on PR....Escape values in phmtl files, rewrite the object manage into method, remove commented out code, move JS out of phtml file.

--- app/code/SMG/Iframes/Block/AddToCart.php
+++ app/code/SMG/Iframes/Block/AddToCart.php
@@ -2,8 +2,22 @@
 namespace SMG\Iframes\Block;
 
 use \Magento\Framework\View\Element\Template;
+use \Magento\Framework\View\Element\Template\Context;
+use \Magento\Store\Model\StoreManagerInterface;
 
 class AddToCart extends Template
 {
+  protected $_storeManager;
 
+  public function __construct(Context $context,
+    StoreManagerInterface $storeManager,
+    array $data = []
+  ) {
+    $this->_storeManager = $storeManager;
+    parent::__construct($context, $data);
+  }
+
+  public function getBaseUrl() {
+    return $this->_storeManager->getStore()->getBaseUrl();
+  }
 }
