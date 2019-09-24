39f500c52443703f50074a4f22dfe8a40a4ccf53
Beth Pauley <elizabeth.pauley@scotts.com>
2019-09-24 14:44:16 -0400

    GOM1-106: Modify controller so that page gets standard head. Doing so means security policy and cart item quantity logic needed to move into block. Updated layout and html to use new block and cartCount method.

--- app/code/SMG/Iframes/Controller/Cart/Index.php
+++ app/code/SMG/Iframes/Controller/Cart/Index.php
@@ -4,37 +4,20 @@ namespace SMG\Iframes\Controller\Cart;
 use \Magento\Framework\App\Action\Action;
 use \Magento\Framework\App\Action\Context;
 use \Magento\Framework\View\Result\PageFactory;
-use \Magento\Checkout\Model\Cart;
-use \SMG\Iframes\Model\ContentSecurityPolicy;
 
 class Index extends Action
 {
   protected $_resultPageFactory;
-  protected $_cart;
-  protected $_contentSecurityPolicy;
 
-  public function __construct(Context $context,
-                              PageFactory $resultPageFactory,
-                              Cart $cart,
-                              ContentSecurityPolicy $contentSecurityPolicy) {
-
+  public function __construct(Context $context, PageFactory $resultPageFactory)
+  {
     $this->_resultPageFactory = $resultPageFactory;
-    $this->_cart = $cart;
-    $this->_contentSecurityPolicy = $contentSecurityPolicy;
     parent::__construct($context);
   }
 
-  public function execute() {
-
-    $this->_contentSecurityPolicy->setContentSecurityPolicy();
-
-    $totalItems = $this->_cart->getQuote()->getItemsSummaryQty();
-
+  public function execute()
+  {
     $resultPage = $this->_resultPageFactory->create();
-    $block = $resultPage->getLayout()
-      ->getBlock('SMG_cart_index')
-      ->setData( 'cartCount' , $totalItems);
-
-    echo $block->toHtml();
+    return $resultPage;
   }
 }
