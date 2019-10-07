e34e60d6704dd12e8df88247f68895b6950c0be0
Nathan VanHoose <hoose145@gmail.com>
2019-10-07 14:35:04 -0400

    ECOM-577: Added code for collecting secure keypad data.

---
+++ app/code/SMG/Vantiv/view/adminhtml/web/js/keypad.js
@@ -0,0 +1,21 @@
+/**
+ * Copyright © 2018 Vantiv, LLC. All rights reserved.
+ * See LICENSE.txt for license details.
+ */
+define([
+        'jquery'
+    ],
+    function ($) {
+        'use strict';
+
+        return function (target) {
+            // $(document).keypress(function (e) {
+            //     alert('Handler for .keypress() called. - ' + event.charCode);
+            // });
+
+            console.log("it worked!");
+
+            return target;
+        };
+    }
+);
