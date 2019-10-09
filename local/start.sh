437bb0cdc137bbeb68c7ab9650d07727f9dd8102
Travis Casper <Travis.Casper@scotts.com>
2019-10-09 13:28:11 -0400

    updated with more automated scripting. And new mount path

--- local/start.sh
+++ local/start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # minikube delete
 LOCAL_MAGENTO_DIR=$(cd .. && pwd)
-minikube start --mount=true --mount-string="${LOCAL_MAGENTO_DIR}:/magento2/" --disk-size='40g' --memory='4g' --extra-config=apiserver.service-node-port-range=80-30000
+minikube start --mount=true --mount-string="${LOCAL_MAGENTO_DIR}:/magento2" --disk-size='40g' --memory='4g' --extra-config=apiserver.service-node-port-range=80-30000
 kubectl apply -k .
+./scripts/build_docker_image.sh
