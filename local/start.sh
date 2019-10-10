f9ad664f85b0b3556b007e45153516940d97a043
Travis Casper <Travis.Casper@scotts.com>
2019-10-10 16:25:29 -0400

    added the brew install to start

--- local/start.sh
+++ local/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# minikube delete
+brew cask install minikube
 LOCAL_MAGENTO_DIR=$(cd .. && pwd)
 minikube start --mount=true --mount-string="${LOCAL_MAGENTO_DIR}:/magento2" --disk-size='40g' --memory='4g' --extra-config=apiserver.service-node-port-range=80-30000
 kubectl apply -k .
