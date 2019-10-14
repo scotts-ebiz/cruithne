89c7cc83a1b53ee7629cae7110231636f9c26169
Sean Kegel <sean.kegel@dynamit.com>
2019-10-14 13:57:25 -0400

    LS-227: Create separate home page and set as default

--- app/code/SMG/SPV2Theming/Setup/Patch/Data/HomePageData.php
+++ app/code/SMG/SPV2Theming/Setup/Patch/Data/HomePageData.php
@@ -2,11 +2,13 @@
 
 namespace SMG\SPV2Theming\Setup\Patch\Data;
 
+use Magento\Cms\Model\PageFactory;
 use Magento\Cms\Model\PageRepository;
 use Magento\Cms\Model\ResourceModel\Page\Collection as PageCollection;
 use Magento\Framework\Setup\ModuleDataSetupInterface;
 use Magento\Framework\Setup\Patch\DataPatchInterface;
 use Magento\Framework\Setup\Patch\PatchRevertableInterface;
+use Magento\Config\Model\ResourceModel\Config;
 
 /**
  * Class HomePageData
@@ -18,37 +20,51 @@ use Magento\Framework\Setup\Patch\PatchRevertableInterface;
 class HomePageData implements DataPatchInterface, PatchRevertableInterface
 {
     private $_moduleDataSetup;
-    private $_pageRepository;
     private $_pageCollection;
+    private $_pageFactory;
+    private $_pageRepository;
+    private $_resourceConfig;
 
     /**
-     * @param  ModuleDataSetupInterface  $moduleDataSetup
-     * @param  PageRepository  $pageRepository
-     * @param  PageCollection  $pageCollection
+     * @param ModuleDataSetupInterface $moduleDataSetup
+     * @param PageCollection $pageCollection
+     * @param PageFactory $pageFactory
+     * @param PageRepository $pageRepository
+     * @param Config $resourceConfig
      */
     public function __construct(
         ModuleDataSetupInterface $moduleDataSetup,
         PageCollection $pageCollection,
-        PageRepository $pageRepository
+        PageFactory $pageFactory,
+        PageRepository $pageRepository,
+        Config $resourceConfig
     ) {
-        /**
-         * If before, we pass $setup as argument in install/upgrade function, from now we start
-         * inject it with DI. If you want to use setup, you can inject it, with the same way as here
-         */
         $this->_moduleDataSetup = $moduleDataSetup;
         $this->_pageCollection = $pageCollection;
+        $this->_pageFactory = $pageFactory;
         $this->_pageRepository = $pageRepository;
+        $this->_resourceConfig = $resourceConfig;
     }
 
     public function apply()
     {
         $this->_moduleDataSetup->getConnection()->startSetup();
 
-        $homePage = $this->_pageCollection->getItemByColumnValue('identifier', 'home');
-        if ($homePage) {
-            $homePage->setData('content', '{{widget type="SMG\SPV2HomePageWidget\Block\Widget\HomePage" heroHeadline="Personalized Lawn Care, Delivered to Your Door" type_name="Scotts Program Home Page"}}');
-            $this->_pageRepository->save($homePage);
+        $homePage = $this->_pageCollection->getItemByColumnValue('identifier', 'scotts-program-v2-home');
+        if (! $homePage) {
+            $homePage = $this->_pageFactory->create()->setData([
+                'title' => 'Scotts Program V2 Home Page',
+                'page_layout' => '1column',
+                'identifier' => 'scotts-program-v2-home',
+                'is_active' => 1,
+            ]);
         }
+
+        $homePage->setData('content', '{{widget type="SMG\SPV2HomePageWidget\Block\Widget\HomePage" heroHeadline="Personalized Lawn Care, Delivered to Your Door" type_name="Scotts Program Home Page"}}');
+        $this->_pageRepository->save($homePage);
+
+        // Set the new page as the default home page.
+        $this->_resourceConfig->saveConfig('web/default/cms_home_page', 'scotts-program-v2-home', 'websites', 1);
 
         $this->_moduleDataSetup->getConnection()->endSetup();
     }
@@ -56,8 +72,16 @@ class HomePageData implements DataPatchInterface, PatchRevertableInterface
     public function revert()
     {
         $this->_moduleDataSetup->getConnection()->startSetup();
-        // Nothing needs to be revert, but it will allow this change to be
-        // applied again each install.
+
+        // Remove page as the default home page.
+        $this->_resourceConfig->deleteConfig('web/default/cms_home_page', 'websites', 1);
+
+        // Delete the page.
+        $homePage = $this->_pageCollection->getItemByColumnValue('identifier', 'scotts-program-v2-home');
+        if ($homePage) {
+            $this->_pageRepository->delete($homePage);
+        }
+
         $this->_moduleDataSetup->getConnection()->endSetup();
     }
 
