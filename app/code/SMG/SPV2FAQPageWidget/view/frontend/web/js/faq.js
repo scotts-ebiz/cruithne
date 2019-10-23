51afd8fcc0beb734cc232444a9ceae04f2acf212
Sean Kegel <sean8@protonmail.com>
2019-10-23 08:43:41 -0400

    LS-356: Add FAQ page (#807)
    * LS-356: Add FAQ page
    
    * LS-356: Allow customization of FAQ categories
    
    * LS-356: Fix tab styling for FAQ categories

---
+++ app/code/SMG/SPV2FAQPageWidget/view/frontend/web/js/faq.js
@@ -0,0 +1,43 @@
+define([
+    'uiComponent',
+    'ko',
+    'jquery',
+    'accordion',
+], function(Component, ko, $, accordion) {
+    return Component.extend({
+        category: ko.observable('faq_1'),
+
+        initialize: function (config) {
+            this._super();
+
+            this.category1Name = config.category1Name;
+            this.category2Name = config.category2Name;
+        },
+
+        setupAccordion() {
+            $('#faq_1').accordion({
+                active: false,
+                collapsible: true,
+                multipleCollapsible: false,
+                openedState: 'active',
+            });
+
+            $('#faq_2').accordion({
+                active: false,
+                collapsible: true,
+                multipleCollapsible: false,
+                openedState: 'active',
+            });
+        },
+
+        setCategory(category) {
+            this.category(category);
+            $('#' + category).accordion({
+                active: false,
+                collapsible: true,
+                multipleCollapsible: false,
+                openedState: 'active',
+            });
+        }
+    });
+});
