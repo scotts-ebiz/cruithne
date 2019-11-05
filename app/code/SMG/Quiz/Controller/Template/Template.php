43d13b944e7210fe20241bd28ee8fd9b50faf204
boris.kuzmanov <boris.kuzmanov@haselt.com>
2019-11-05 10:48:07 +0100

    Use the API endpoint for a new quiz that is stored in the Recommendations module

--- app/code/SMG/Quiz/Controller/Template/Template.php
+++ app/code/SMG/Quiz/Controller/Template/Template.php
@@ -6,21 +6,29 @@ use GuzzleHttp\Client;
 use Magento\Framework\App\Action\Action;
 use Magento\Framework\App\Action\Context;
 use Magento\Framework\Controller\ResultFactory;
+use SMG\Recommendations\Helper\QuizHelper as RecommendationsQuizHelper;
 
 class Template extends Action
 {
     private $_resultFactory;
 
-    public function __construct(Context $context, ResultFactory $resultFactory)
+    protected $_helper;
+
+    public function __construct(Context $context, ResultFactory $resultFactory, RecommendationsQuizHelper $helper)
     {
         parent::__construct($context);
         $this->_resultFactory = $resultFactory;
+        $this->_helper = $helper;
     }
 
     public function execute()
     {
+        if( ! $this->_helper->getNewQuizApiPath() ) {
+            return;
+        }
+
         $http = new Client();
-        $results = $http->get('https://lspaasdraft.azurewebsites.net/api/quizzes/template');
+        $results = $http->get(filter_var($this->_helper->getNewQuizApiPath(), FILTER_SANITIZE_URL));
         $json = $results->getBody()->getContents();
 
         $result = $this->_resultFactory->create(ResultFactory::TYPE_JSON);
