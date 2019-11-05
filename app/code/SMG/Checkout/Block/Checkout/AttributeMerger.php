d0a1ee71883f7e992ebe3ea4e45838bc9644d013
Ian Gall <ian.gall@scotts.com>
2019-11-05 10:52:35 -0500

    Adding class and function to am php code in SMG

--- app/code/SMG/Checkout/Block/Checkout/AttributeMerger.php
+++ app/code/SMG/Checkout/Block/Checkout/AttributeMerger.php
@@ -1,54 +1,71 @@
 <?php
-namespace SMG\Checkout\Plugin\Block\Checkout;
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace SMG\Checkout\Block\Checkout;
+
+use Magento\Customer\Api\CustomerRepositoryInterface as CustomerRepository;
+use Magento\Customer\Api\Data\CustomerInterface;
+use Magento\Customer\Helper\Address as AddressHelper;
+use Magento\Customer\Model\Session;
+use Magento\Directory\Helper\Data as DirectoryHelper;
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Framework\Exception\NoSuchEntityException;
+
+/**
+ * Cart source
+ *
+ * @SuppressWarnings(PHPMD.CookieAndSessionMisuse)
+ */
 class AttributeMerger
 {
-
-
-    protected function afterGetMultilineFieldConfig(\Magento\Checkout\Block\Checkout\AttributeMerger $result, $attributeCode, array $attributeConfig, $providerName,
-$dataScopePrefix)
+    protected function getMultilineFieldConfig($attributeCode, array $attributeConfig, $providerName, $dataScopePrefix)
     {
         $lines = [];
         unset($attributeConfig['validation']['required-entry']);
         for ($lineIndex = 0; $lineIndex < (int)$attributeConfig['size']; $lineIndex++) {
-            $isFirstLine = $lineIndex === 0;
-            $line = [
-                'label' => __("%1: Lineasdf %2", $attributeConfig['label'], $lineIndex + 1),
-                'component' => 'Magento_Ui/js/form/element/abstract',
-                'config' => [
-                    // customScope is used to group elements within a single form e.g. they can be validated separately
-                    'customScope' => $dataScopePrefix,
-                    'template' => 'ui/form/field',
-                    'elementTmpl' => 'ui/form/element/input'
-                ],
-                'dataScope' => $lineIndex,
-                'provider' => $providerName,
-                'validation' => $isFirstLine
-                    ? array_merge(
-                        ['required-entry' => (bool)$attributeConfig['required']],
-                        $attributeConfig['validation']
-                    )
-                : $attributeConfig['validation'],
-                'additionalClasses' => $isFirstLine ? 'field' : 'additional'
-            ];
-            if ($isFirstLine && isset($attributeConfig['default']) && $attributeConfig['default'] != null) {
-                $line['value'] = $attributeConfig['default'];
-            }
-            $lines[] = $line;
+          $isFirstLine = $lineIndex === 0;
+          $line = [
+            'label' => __("%1: Lineasdf %2", $attributeConfig['label'], $lineIndex + 1),
+            'component' => 'Magento_Ui/js/form/element/abstract',
+            'config' => [
+              // customScope is used to group elements within a single form e.g. they can be validated separately
+              'customScope' => $dataScopePrefix,
+              'template' => 'ui/form/field',
+              'elementTmpl' => 'ui/form/element/input'
+            ],
+            'dataScope' => $lineIndex,
+            'provider' => $providerName,
+            'validation' => $isFirstLine
+              ? array_merge(
+                ['required-entry' => (bool)$attributeConfig['required']],
+                $attributeConfig['validation']
+              )
+              : $attributeConfig['validation'],
+            'additionalClasses' => $isFirstLine ? 'field' : 'additional'
+
+          ];
+          if ($isFirstLine && isset($attributeConfig['default']) && $attributeConfig['default'] != null) {
+            $line['value'] = $attributeConfig['default'];
+          }
+          $lines[] = $line;
         }
         return [
-            'component' => 'Magento_Ui/js/form/components/group',
-            'label' => $attributeConfig['label'],
-            'required' => (bool)$attributeConfig['required'],
-            'dataScope' => $dataScopePrefix . '.' . $attributeCode,
-            'provider' => $providerName,
-            'sortOrder' => $attributeConfig['sortOrder'],
-            'type' => 'group',
-            'config' => [
-                'template' => 'ui/group/group',
-                'additionalClasses' => $attributeCode
-            ],
-            'children' => $lines,
+          'component' => 'Magento_Ui/js/form/components/group',
+          'label' => $attributeConfig['label'],
+          'required' => (bool)$attributeConfig['required'],
+          'dataScope' => $dataScopePrefix . '.' . $attributeCode,
+          'provider' => $providerName,
+          'sortOrder' => $attributeConfig['sortOrder'],
+          'type' => 'group',
+          'config' => [
+            'template' => 'ui/group/group',
+            'additionalClasses' => $attributeCode
+          ],
+          'children' => $lines,
         ];
-        return $result;
     }
+
 }
