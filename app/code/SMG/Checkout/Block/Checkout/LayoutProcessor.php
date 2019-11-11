77dc8fa383ef05577c3a5c8ca0f2d533851787ca
Ian Gall <ian.gall@scotts.com>
2019-11-11 17:15:50 -0500

    Created override layoutprocessor class file

---
+++ app/code/SMG/Checkout/Block/Checkout/LayoutProcessor.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace SMG\Checkout\Block\Checkout;
+
+use Magento\Checkout\Helper\Data;
+use Magento\Framework\App\ObjectManager;
+use Magento\Store\Model\StoreManagerInterface;
+
+/**
+ * Class LayoutProcessor
+ */
+class LayoutProcessor extends \Magento\Checkout\Block\Checkout\LayoutProcessor
+{
+
+
+}
