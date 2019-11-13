7e163fdca446eeedae664d45db2126bc9e28d73f
Sean Kegel <sean.kegel@dynamit.com>
2019-11-13 12:36:14 -0500

    LS-479: Redirect to results page at the end of the quiz
    LS-513: Save quiz responses to session storage

---
+++ app/code/SMG/Recommendations/Controller/Results/Index.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace SMG\Recommendations\Controller\Results;
+
+use Magento\Framework\App\Action\Action;
+use Magento\Framework\App\Action\Context;
+use Magento\Framework\View\Result\PageFactory;
+
+class Index extends Action
+{
+    /**
+     * @var PageFactory
+     */
+    protected $_resultPageFactory;
+
+    public function __construct(
+        Context $context,
+        PageFactory $resultPageFactory
+    ) {
+        parent::__construct($context);
+
+        $this->_resultPageFactory = $resultPageFactory;
+    }
+
+    public function execute()
+    {
+        $resultPage = $this->_resultPageFactory->create();
+
+        return $resultPage;
+    }
+}
