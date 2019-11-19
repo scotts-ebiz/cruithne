55604558b6d541bd070dc720753392d8e05872ce
boris.kuzmanov <boris.kuzmanov@haselt.com>
2019-11-19 14:38:12 +0100

    LS-557: Create method for calling the mapToUser API endpoint when customer log in

--- app/code/SMG/SPV2CheckoutLogin/Controller/LoginPost.php
+++ app/code/SMG/SPV2CheckoutLogin/Controller/LoginPost.php
@@ -24,4 +24,35 @@ class LoginPost
 		return $result;
 	}
 
+	private function mapCompletedQuiz($user_id, $quiz_id)
+	{
+		if( empty( $user_id ) || empty( $quiz_id ) ) {
+			return;
+		}
+
+		try {
+			
+			$url = 'https://lspaasdraft.azurewebsites.net/api/completedQuizzes/mapToUser';
+
+			$ch = curl_init();
+			curl_setopt( $ch, CURLOPT_URL, $url );
+			curl_setopt( $ch, CURLOPT_CUSTOMREQUEST, 'PUT' );
+			curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
+			curl_setopt( $ch, CURLOPT_HTTPHEADER, array(
+				'Content-Type: application/json',
+				'x-userid: ' . $user_id,
+			) );
+			curl_setopt( $ch, CURLOPT_POSTFIELDS, array( $quiz_id ) );
+
+			$response = curl_exec( $ch );
+			curl_close( $ch );
+
+			return $response;
+		} catch(Exception $e) {
+			echo $e->getMessage() . ' (' . $e->getCode() . ')';
+		}
+
+
+	}
+
 }
