0005c5a4c8ace3329f3345b07bd726db8f15908f
boris.kuzmanov <boris.kuzmanov@haselt.com>
2019-11-19 13:07:24 +0100

    Redirect non-logged users from checkout to login. On successful login redirect back to checkout

--- app/code/SMG/SPV2CheckoutLogin/Controller/Checkout/Index.php
+++ app/code/SMG/SPV2CheckoutLogin/Controller/Checkout/Index.php
@@ -11,24 +11,31 @@ class Index extends \Magento\Checkout\Controller\Index\Index implements HttpGetA
     {
 
         $checkoutHelper = $this->_objectManager->get(\Magento\Checkout\Helper\Data::class);
-        if (!$checkoutHelper->canOnepageCheckout()) {
-            $this->messageManager->addErrorMessage(__('One-page checkout is turned off.'));
-            return $this->resultRedirectFactory->create()->setPath('checkout/cart');
-        }
-
+        $urlHelper = $this->_objectManager->get(\Magento\Framework\Url\Helper\Data::class);
         $quote = $this->getOnepage()->getQuote();
-        if (!$quote->hasItems() || $quote->getHasError() || !$quote->validateMinimumAmount()) {
-            return $this->resultRedirectFactory->create()->setPath('checkout/cart');
+
+        /**
+         * If the customer is not logged in and guest checkout is not allowed,
+         * redirect the customer to the login page. Set current URL (/checkout) as referer,
+         * so the customer is redirected to checkout page on successfull login.
+         */
+        if( ! $this->_customerSession->isLoggedIn() && ! $checkoutHelper->isAllowedGuestCheckout( $quote ) ) {
+            $resultRedirect = $this->resultRedirectFactory->create();
+
+            $customerLoginUrl = $this->_url->getUrl( 
+                'customer/account/login',
+                array(
+                    'referer' => $urlHelper->getEncodedUrl( $this->_url->getCurrentUrl() )
+                )
+            );
+
+            return $resultRedirect->setPath($customerLoginUrl);
         }
 
-        // Redirect to login page if customer is not logged in
-        if (!$this->_customerSession->isLoggedIn() && !$checkoutHelper->isAllowedGuestCheckout($quote)) {
-            return $this->resultRedirectFactory->create()->setPath('customer/account/login');
-        }
-
-        if (!$this->isSecureRequest()) {
+        if ( ! $this->isSecureRequest() ) {
             $this->_customerSession->regenerateId();
         }
+
         $this->_objectManager->get(\Magento\Checkout\Model\Session::class)->setCartWasUpdated(false);
         $this->getOnepage()->initCheckout();
         $resultPage = $this->resultPageFactory->create();
--- app/code/SMG/SPV2CheckoutLogin/Controller/LoginPost.php
+++ app/code/SMG/SPV2CheckoutLogin/Controller/LoginPost.php
@@ -5,16 +5,11 @@ namespace SMG\SPV2CheckoutLogin\Controller;
 class LoginPost
 {
 
-	/**
-	 * Redirect customer to checkout page after login
-	 */
 	public function afterExecute(
 		\Magento\Customer\Controller\Account\LoginPost $subject,
 		$result
 	)
 	{
-		$result->setPath('checkout');
-
 		return $result;
 	}
 
