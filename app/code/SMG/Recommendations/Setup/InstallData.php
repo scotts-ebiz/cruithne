390aaf24850b81fc31f26fa311085dddd43a398e
boris.kuzmanov <boris.kuzmanov@haselt.com>
2019-11-21 13:24:53 +0100

    Add is_addon custom field for the products
    It will be used for showing/hiding products in the order summary block in checkout

---
+++ app/code/SMG/Recommendations/Setup/InstallData.php
@@ -0,0 +1,46 @@
+<?php
+namespace SMG\Recommendations\Setup;
+
+use Magento\Eav\Setup\EavSetup;
+use Magento\Eav\Setup\EavSetupFactory;
+use Magento\Framework\Setup\InstallDataInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+
+class InstallData implements InstallDataInterface
+{
+	private $_eavSetupFactory;
+
+	public function __construct(
+		EavSetupFactory $eavSetupFactory
+	)
+	{
+		$this->_eavSetupFactory = $eavSetupFactory;
+	}
+	
+	public function install(
+		ModuleDataSetupInterface $setup,
+		ModuleContextInterface $context
+	)
+	{
+		$eavSetup = $this->_eavSetupFactory->create( [ 'setup' => $setup ] );
+
+		$eavSetup->addAttribute(
+             \Magento\Catalog\Model\Product::ENTITY,
+            'is_addon', [
+				'type' 			=> 'int',
+	            'label' 		=> 'Is Addon Product?',
+	            'input' 		=> 'boolean',                
+	            'source' 		=> 'Magento\Eav\Model\Entity\Attribute\Source\Boolean',                
+	            'global' 		=> \Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface::SCOPE_GLOBAL,
+	            'visible' 		=> true,
+	            'required' 		=> false,
+	            'user_defined' 	=> false,
+	            'default' 		=> null,
+	            'group' 		=> 'Custom Fields',
+	            'backend' 		=> ''
+			 ]
+        );
+
+	}
+}
