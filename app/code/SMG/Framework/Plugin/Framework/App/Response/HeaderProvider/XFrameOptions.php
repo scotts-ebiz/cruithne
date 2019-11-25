9caa9219bcadd7e0c096cedcafeecbd682472eca
Beth Pauley <elizabeth.pauley@scotts.com>
2019-11-25 11:37:48 -0500

    GOM1-141: Move x-frame-options into its own module to align with Magento structure that we are modifying the logic of.

--- app/code/SMG/Framework/Plugin/Framework/App/Response/HeaderProvider/XFrameOptions.php
+++ app/code/SMG/Framework/Plugin/Framework/App/Response/HeaderProvider/XFrameOptions.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace SMG\Iframes\Plugin\Framework\App\Response\HeaderProvider;
+namespace SMG\Framework\Plugin\Framework\App\Response\HeaderProvider;
 
 /**
  * Adds an X-FRAME-OPTIONS header to HTTP responses to safeguard against click-jacking.
