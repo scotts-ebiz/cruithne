4b8ee400bc65aaced5f66933a87f8854329679b9
Wes Crow <wes.crow@scotts.com>
2019-11-26 08:38:35 -0500

    Renaming Quiz module to RecommendationQuiz

--- app/code/SMG/RecommendationQuiz/Controller/Index/Index.php
+++ app/code/SMG/RecommendationQuiz/Controller/Index/Index.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace SMG\Quiz\Controller\Index;
+namespace SMG\RecommendationQuiz\Controller\Index;
 
 use Magento\Framework\App\Action\Action;
 use Magento\Framework\App\Action\Context;
