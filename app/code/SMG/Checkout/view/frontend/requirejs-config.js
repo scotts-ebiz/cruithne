d09ce43963eec4ea6e3dba28bdc18a373e57ec5d
Ian Gall <ian.gall@scotts.com>
2019-12-02 16:13:31 -0500

    Adding email override js file

--- app/code/SMG/Checkout/view/frontend/requirejs-config.js
+++ app/code/SMG/Checkout/view/frontend/requirejs-config.js
@@ -1,7 +1,8 @@
 var config = {
     map: {
         '*': {
-            ajaxQty: 'MageMonkey_AutoUpdateCartAjax/js/cartQtyUpdate'
+            ajaxQty: 'MageMonkey_AutoUpdateCartAjax/js/cartQtyUpdate',
+            'Magento_Checkout/js/view/form/element/email': 'SMG_Checkout/js/view/form/element/email-override'
         }
     }
 };
