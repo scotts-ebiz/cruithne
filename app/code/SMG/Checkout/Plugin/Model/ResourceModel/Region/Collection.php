71b3540bc1942bb583b4363b2e97ced3ad8a9c4c
Ian Gall <ian.gall@scotts.com>
2019-12-04 10:02:24 -0500

    Added new Magento Directory override and i18n dir and new csv file

--- app/code/SMG/Checkout/Plugin/Model/ResourceModel/Region/Collection.php
+++ app/code/SMG/Checkout/Plugin/Model/ResourceModel/Region/Collection.php
@@ -2,30 +2,30 @@
 namespace SMG\Checkout\Plugin\Model\ResourceModel\Region;
 
 class Collection {
-public function afterToOptionArray()
-    {
-        $options = [];
-        $propertyMap = [
-            'value' => 'region_id',
-            'title' => 'default_name',
-            'country_id' => 'country_id',
-        ];
+//  public function afterToOptionArray(array $result) {
+//        $options = [];
+//        $propertyMap = [
+//            'value' => 'region_id',
+//            'title' => 'default_name',
+//            'country_id' => 'country_id',
+//        ];
+//
+//        foreach ($this as $item) {
+//            $option = [];
+//            foreach ($propertyMap as $code => $field) {
+//                $option[$code] = $item->getData($field);
+//            }
+//            $option['label'] = $item->getName();
+//            $options[] = $option;
+//        }
+//
+//        if (count($options) > 0) {
+//            array_unshift(
+//                $options,
+//                ['title' => '', 'value' => '', 'label' => __('Select State')]
+//            );
+//        }
+//        return $result;
+//    }
 
-        foreach ($this as $item) {
-            $option = [];
-            foreach ($propertyMap as $code => $field) {
-                $option[$code] = $item->getData($field);
-            }
-            $option['label'] = $item->getName();
-            $options[] = $option;
-        }
-
-        if (count($options) > 0) {
-            array_unshift(
-                $options,
-                ['title' => '', 'value' => '', 'label' => __('Select State')]
-            );
-        }
-        return $options;
-    }
 }
