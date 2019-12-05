69a91f2a747a13a1cb6443e984b2bfdfa41c1a58
nixondev <cnixon@lokion.com>
2019-12-05 15:27:23 -0600

    GOM1-121 added header providers for Content Security Policy

--- app/code/SMG/Iframes/Block/HeaderCart.php
+++ app/code/SMG/Iframes/Block/HeaderCart.php
@@ -4,22 +4,17 @@ namespace SMG\Iframes\Block;
 use \Magento\Framework\View\Element\Template;
 use \Magento\Framework\View\Element\Template\Context;
 use \Magento\Checkout\Model\Cart;
-use \SMG\Iframes\Model\ContentSecurityPolicy;
 
 class HeaderCart extends Template
 {
 
   protected $_cart;
-  protected $_contentSecurityPolicy;
 
   public function __construct(
     Context $context,
-    Cart $cart,
-    ContentSecurityPolicy $contentSecurityPolicy)
+    Cart $cart)
   {
     $this->_cart = $cart;
-    $this->_contentSecurityPolicy = $contentSecurityPolicy;
-    $this->_contentSecurityPolicy->setContentSecurityPolicy();
     return parent::__construct($context);
   }
 
