05312a3d4c8528ce03007e7d7b200ecb46b7437e
Wes Crow <wes.crow@scotts.com>
2019-12-05 16:50:37 -0500

    Removing object manager for checkout index interceptor.

--- app/code/SMG/SubscriptionCheckout/Controller/Checkout/Index.php
+++ app/code/SMG/SubscriptionCheckout/Controller/Checkout/Index.php
@@ -6,19 +6,76 @@ use Magento\Framework\App\Action\HttpGetActionInterface as HttpGetActionInterfac
 
 class Index extends \Magento\Checkout\Controller\Index\Index implements HttpGetActionInterface
 {
+    /**
+     * @var \Magento\Checkout\Helper\Data 
+     */
+    protected $_checkoutHelper;
 
+    /**
+     * @var \Magento\Framework\Url\Helper\Data 
+     */
+    protected $_urlHelper;
+
+    /**
+     * Index constructor.
+     * @param \Magento\Backend\Block\Template\Context $context
+     * @param \Magento\Checkout\Helper\Data $checkoutHelper
+     * @param \Magento\Framework\Url\Helper\Data $urlHelper
+     * @param array $data
+     */
+    public function __construct(
+        \Magento\Framework\App\Action\Context $context,
+        \Magento\Customer\Model\Session $customerSession,
+        \Magento\Customer\Api\CustomerRepositoryInterface $customerRepository,
+        \Magento\Customer\Api\AccountManagementInterface $accountManagement,
+        \Magento\Framework\Registry $coreRegistry,
+        \Magento\Framework\Translate\InlineInterface $translateInline,
+        \Magento\Framework\Data\Form\FormKey\Validator $formKeyValidator,
+        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
+        \Magento\Framework\View\LayoutFactory $layoutFactory,
+        \Magento\Quote\Api\CartRepositoryInterface $quoteRepository,
+        \Magento\Framework\View\Result\PageFactory $resultPageFactory,
+        \Magento\Framework\View\Result\LayoutFactory $resultLayoutFactory,
+        \Magento\Framework\Controller\Result\RawFactory $resultRawFactory,
+        \Magento\Framework\Controller\Result\JsonFactory $resultJsonFactory,
+        \Magento\Checkout\Helper\Data $checkoutHelper,
+        \Magento\Framework\Url\Helper\Data $urlHelper,
+        array $data = []
+    ) {
+        $this->_checkoutHelper = $checkoutHelper;
+        $this->_urlHelper = $urlHelper;
+        parent::__construct(
+            $context,
+            $customerSession,
+            $customerRepository,
+            $accountManagement,
+            $coreRegistry,
+            $translateInline,
+            $formKeyValidator,
+            $scopeConfig,
+            $layoutFactory,
+            $quoteRepository,
+            $resultPageFactory,
+            $resultLayoutFactory,
+            $resultRawFactory,
+            $resultJsonFactory,
+            $data
+        );
+    }
+
+    /**
+     * @return \Magento\Framework\App\ResponseInterface|\Magento\Framework\Controller\Result\Redirect|\Magento\Framework\Controller\ResultInterface|\Magento\Framework\View\Result\Page
+     */
     public function execute()
     {
-        $checkoutHelper = $this->_objectManager->get(\Magento\Checkout\Helper\Data::class);
-        $urlHelper = $this->_objectManager->get(\Magento\Framework\Url\Helper\Data::class);
         $quote = $this->getOnepage()->getQuote();
 
         /**
          * If the customer is not logged in and guest checkout is not allowed,
          * redirect the customer to the login page. Set current URL (/checkout) as referer,
-         * so the customer is redirected to checkout page on successfull login.
+         * so the customer is redirected to checkout page on successful login.
          */
-        if( ! $this->_customerSession->isLoggedIn() && ! $checkoutHelper->isAllowedGuestCheckout( $quote ) ) {
+        if( ! $this->_customerSession->isLoggedIn() && ! $this->_checkoutHelper->isAllowedGuestCheckout( $quote ) ) {
             $resultRedirect = $this->resultRedirectFactory->create();
 
             $params = array(
@@ -28,7 +85,7 @@ class Index extends \Magento\Checkout\Controller\Index\Index implements HttpGetA
             $customerLoginUrl = $this->_url->getUrl( 
                 'customer/account/login',
                 array(
-                    'referer'   => $urlHelper->getEncodedUrl( $this->_url->getCurrentUrl() ),
+                    'referer'   => $this->_urlHelper->getEncodedUrl( $this->_url->getCurrentUrl() ),
                     '_query'    => $params
                 )
             );
@@ -47,6 +104,9 @@ class Index extends \Magento\Checkout\Controller\Index\Index implements HttpGetA
         return $resultPage;
     }
 
+    /**
+     * @return bool
+     */
     private function isSecureRequest(): bool
     {
         $request = $this->getRequest();
