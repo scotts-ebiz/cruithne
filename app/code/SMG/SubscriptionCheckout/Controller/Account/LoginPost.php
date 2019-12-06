f372b36b7101f613ff4abe3c235c019375192d1f
boris.kuzmanov <boris.kuzmanov@haselt.com>
2019-12-06 16:13:49 +0100

    Update mapToUser function on customer login

--- app/code/SMG/SubscriptionCheckout/Controller/Account/LoginPost.php
+++ app/code/SMG/SubscriptionCheckout/Controller/Account/LoginPost.php
@@ -6,12 +6,18 @@ class LoginPost
 {
 
 	protected $_request;
+	protected $_recommendationHelper;
+	protected $_customerSession;
 
 	public function __construct(
-		\Magento\Framework\App\RequestInterface $request
+		\Magento\Framework\App\RequestInterface $request,
+		\SMG\RecommendationApi\Helper\RecommendationHelper $recommendationHelper,
+		\Magento\Customer\Model\Session $customerSession
 	)
 	{
 		$this->_request = $request;
+		$this->_recommendationHelper = $recommendationHelper;
+		$this->_customerSession = $customerSession;
 	}
 
 	public function afterExecute(
@@ -19,40 +25,49 @@ class LoginPost
 		$result
 	)
 	{
-		//print_r( $this->_request->getParam('quiz_id') );
+
+		if( ! empty( $this->_request->getParam('quiz_id' ) ) ) {
+			$this->mapToUser($this->_customerSession->getCustomer()->getId(), $this->_request->getParam('quiz_id'));
+		}
 
 		return $result;
 	}
 
-	private function mapCompletedQuiz($user_id, $quiz_id)
-	{
-		if( empty( $user_id ) || empty( $quiz_id ) ) {
-			return;
-		}
+	private function mapToUser($user_id, $quiz_id)
+    {
+        // Make sure we have a path
+        if ( ! $this->_recommendationHelper->getMapToUserPath() ) {
+            return;
+        }
 
-		try {
-			
-			$url = 'https://lspaasdraft.azurewebsites.net/api/completedQuizzes/mapToUser';
+        // Make sure that customer id and quiz id exist
+        if (empty($user_id) || empty($quiz_id)) {
+            return;
+        }
 
-			$ch = curl_init();
-			curl_setopt( $ch, CURLOPT_URL, $url );
-			curl_setopt( $ch, CURLOPT_CUSTOMREQUEST, 'PUT' );
-			curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
-			curl_setopt( $ch, CURLOPT_HTTPHEADER, array(
-				'Content-Type: application/json',
-				'x-userid: ' . $user_id,
-			) );
-			curl_setopt( $ch, CURLOPT_POSTFIELDS, array( $quiz_id ) );
+        // Get customer Gigya ID
+        $gigyaId = $this->_customerSession->getCustomer()->getGigyaUid();
 
-			$response = curl_exec( $ch );
-			curl_close( $ch );
+        try {
+            $url = filter_var($this->_recommendationHelper->getMapToUserPath(), FILTER_SANITIZE_URL);
 
-			return $response;
-		} catch(Exception $e) {
-			echo $e->getMessage() . ' (' . $e->getCode() . ')';
-		}
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, $url);
+            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PUT');
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, [
+                'Content-Type: application/json',
+                'x-userid: ' . $gigyaId,
+            ]);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, [$quiz_id]);
 
+            $response = curl_exec($ch);
+            curl_close($ch);
 
-	}
+            return $response;
+        } catch (Exception $e) {
+            echo $e->getMessage() . ' (' . $e->getCode() . ')';
+        }
+    }
 
 }
