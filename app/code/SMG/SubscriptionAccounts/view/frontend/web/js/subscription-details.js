00ef396b537f9b6e42e873048c8a5966025fad87
boris.kuzmanov <boris.kuzmanov@haselt.com>
2019-12-19 12:36:15 +0100

    Bind click events using KO

--- app/code/SMG/SubscriptionAccounts/view/frontend/web/js/subscription-details.js
+++ app/code/SMG/SubscriptionAccounts/view/frontend/web/js/subscription-details.js
@@ -16,26 +16,17 @@ define([
 
     return Component.extend({
         initialize(config) {
-            console.log(config);
             this.subscriptions = ko.observable(config.subscriptions);
-            this.invoices = ko.observable(config.invoices);
-            let self = this;
-
-            $(document).on('click', '.cancel-subscription-modal', function(e) {
-                e.preventDefault();
-                $('.modal-popup--subscriptions').modal(options).modal('openModal');
-            });
-
-            $(document).on('click', '.close-subscription-modal', function(e) {
-                e.preventDefault();
-                $('.modal-popup--subscriptions').modal(options).modal('closeModal');
-            });
-
-            $(document).on('click', 'button#cancelSubscription', function() {
-                self.cancelSubscription();
-            });
+        },
+        displaySubscriptionModal: function() {
+            $('.modal-popup--subscriptions').modal(options).modal('openModal');
+        },
+        closeSubscriptionModal: function() {
+            $('.modal-popup--subscriptions').modal(options).modal('closeModal');
         },
         cancelSubscription: function() {
+            let self = this;
+
             $.ajax({
                 type: 'POST',
                 url: window.location.origin + '/rest/V1/subscription/cancel',
@@ -45,10 +36,10 @@ define([
                 success: function(response) {
                     var response = JSON.parse(response);
                     if( response.success === true ) {
-                        $('.modal-popup--subscriptions').modal('closeModal');
+                        self.closeSubscriptionModal();
                     } else {
                         alert( response.message );
-                        $('.modal-popup--subscriptions').modal('closeModal');
+                        self.closeSubscriptionModal();
                     }
                 }
             })
