6b0ec2b1ea56a2183b4b1eea0e1126ce85315b98
Sean Kegel <sean.kegel@dynamit.com>
2019-12-20 16:18:31 -0500

    Update checkout plugin for login redirect

--- app/code/SMG/SubscriptionCheckout/Plugin/Controller/Index/Index.php
+++ app/code/SMG/SubscriptionCheckout/Plugin/Controller/Index/Index.php
@@ -60,12 +60,12 @@ class Index
      */
     protected $_urlHelper;
 
-
     /**
      * Index constructor.
      *
      */
-    public function __construct(LoggerInterface $logger,
+    public function __construct(
+        LoggerInterface $logger,
         SubscriptionHelper $subscriptionHelper,
         StoreManagerInterface $storeManager,
         CustomerSession $customerSession,
@@ -73,8 +73,8 @@ class Index
         RedirectFactory $resultRedirectFactory,
         CoreSession $coreSession,
         UrlInterface $url,
-        UrlHelper $urlHelper)
-    {
+        UrlHelper $urlHelper
+    ) {
         $this->_logger = $logger;
         $this->_subscriptionHelper = $subscriptionHelper;
         $this->_storeManager = $storeManager;
@@ -90,15 +90,14 @@ class Index
      * Check to see if it is a subscription and if the user is logged in before continuing
      *
      * @param \Magento\Checkout\Controller\Index\Index $subject
+     * @param callable $proceed
      * @return Redirect
      */
-    public function beforeExecute(\Magento\Checkout\Controller\Index\Index $subject)
+    public function aroundExecute(\Magento\Checkout\Controller\Index\Index $subject, callable $proceed)
     {
-        try
-        {
+        try {
             // if this store uses subscription then check for login before continuing
-            if ($this->_subscriptionHelper->isActive($this->_storeManager->getStore()->getId()))
-            {
+            if ($this->_subscriptionHelper->isActive($this->_storeManager->getStore()->getId())) {
                 // get the onepage quote to see if the user is a logged in user or a guest user
                 $quote = $subject->getOnepage()->getQuote();
 
@@ -107,29 +106,28 @@ class Index
                  * redirect the customer to the login page. Set current URL (/checkout) as referer,
                  * so the customer is redirected to checkout page on successful login.
                  */
-                if (!$this->_customerSession->isLoggedIn() && !$this->_checkoutHelper->isAllowedGuestCheckout($quote))
-                {
+                if (!$this->_customerSession->isLoggedIn() && !$this->_checkoutHelper->isAllowedGuestCheckout($quote)) {
                     $resultRedirect = $this->_resultRedirectFactory->create();
 
-                    $params = array(
+                    $params = [
                         'quiz_id' => $this->_coreSession->getQuizId()
-                    );
+                    ];
 
                     $customerLoginUrl = $this->_url->getUrl(
                         'customer/account/login',
-                        array(
+                        [
                             'referer' => $this->_urlHelper->getEncodedUrl($this->_url->getCurrentUrl()),
                             '_query' => $params
-                        )
+                        ]
                     );
 
                     // return the login page
                     return $resultRedirect->setPath($customerLoginUrl);
                 }
             }
-        }
-        catch (\Exception $e)
-        {
+
+            return $proceed();
+        } catch (\Exception $e) {
             $this->_logger->error($e);
         }
     }
