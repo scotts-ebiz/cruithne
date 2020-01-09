41ab039b181a5030ebf5aadcf0d198cd7037fe67
boris.kuzmanov <boris.kuzmanov@haselt.com>
2020-01-09 16:41:15 +0100

    Wait for Gigya to load before displaying content

--- app/design/frontend/Scottsbase/scottsprogram-v2/web/js/gigya.js
+++ app/design/frontend/Scottsbase/scottsprogram-v2/web/js/gigya.js
@@ -1,23 +1,20 @@
-require(['jquery'], function($){
+require(['jquery', 'Gigya_GigyaIM/js/gigya_script'], function($, gigya){
 
-	window.onGigyaServiceReady = function () {
-		alert('ready')
+	window.onGigyaServiceReady = function (serviceName) {
+		gigya.Functions.performGigyaActions();
+		$('.after-gigya-login').css( { 'display': 'block' } );
+
+		$(document).on('click', '.gigya-composite-control-link', function() {
+			setTimeout( function() {
+				if( $('.gigya-screen-content .gigya-register-form').length > 0 ) {
+					$('.after-gigya-register').css({'display':'block'})
+					$('.after-gigya-login').css({'display':'none'})
+				} else {
+					$('.after-gigya-login').css({'display':'block'})
+					$('.after-gigya-register').css({'display':'none'})
+				}
+			}, 1000)
+		})
 	};
-
-	function changeAfterGigyaContent() {
-		setTimeout(function() {
-			console.log($('.gigya-screen-content .gigya-register-form').length);
-			if( $('.gigya-screen-content .gigya-register-form').length > 0 ) {
-				$('.after-gigya-register').css({'display':'block'})
-				$('.after-gigya-login').css({'display':'none'})
-			} else {
-				$('.after-gigya-login').css({'display':'block'})
-				$('.after-gigya-register').css({'display':'none'})
-			}
-		}, 3000)
-	}
-
-	$(document).on('click', '.gigya-composite-control-link', function() {
-		changeAfterGigyaContent();
-	})
+	
 });
