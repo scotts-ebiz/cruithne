0113a0ef3a0fe37f97d89c4f36d0bf1791f98165
Ian Gall <ian.gall@scotts.com>
2020-01-20 21:19:24 -0500

    Replacing REM with PX for the btn size

--- app/code/SMG/HeroCallout/Model/Source/HeroCalloutBannerTextSize.php
+++ app/code/SMG/HeroCallout/Model/Source/HeroCalloutBannerTextSize.php
@@ -4,22 +4,22 @@ namespace SMG\HeroCallout\Model\Source;
 
 class HeroCalloutBannerTextSize implements \Magento\Framework\Option\ArrayInterface
 {
-  public function toOptionArray()
-  {
-    return [
-      ['value' => 'font-size: 8px; ', 'label' => __('8px')],
-      ['value' => 'font-size: 9px; ', 'label' => __('9px')],
-      ['value' => 'font-size: 10px; ', 'label' => __('10px')],
-      ['value' => 'font-size: 11px; ', 'label' => __('11px')],
-      ['value' => 'font-size: 12px; ', 'label' => __('12px')],
-      ['value' => 'font-size: 13px; ', 'label' => __('13px')],
-      ['value' => 'font-size: 14px; ', 'label' => __('14px')],
-      ['value' => 'font-size: 15px; ', 'label' => __('15px')],
-      ['value' => 'font-size: 16px; ', 'label' => __('16px')],
-      ['value' => 'font-size: 17px; ', 'label' => __('17px')],
-      ['value' => 'font-size: 18px; ', 'label' => __('18px')],
-      ['value' => 'font-size: 19px; ', 'label' => __('19px')],
-      ['value' => 'font-size: 20px; ', 'label' => __('20px')]
-    ];
-  }
+    public function toOptionArray()
+    {
+        return [
+            ['value' => 'font-size: 8px; ', 'label' => __('8px')],
+            ['value' => 'font-size: 9px; ', 'label' => __('9px')],
+            ['value' => 'font-size: 10px; ', 'label' => __('10px')],
+            ['value' => 'font-size: 11px; ', 'label' => __('11px')],
+            ['value' => 'font-size: 12px; ', 'label' => __('12px')],
+            ['value' => 'font-size: 13px; ', 'label' => __('13px')],
+            ['value' => 'font-size: 14px; ', 'label' => __('14px')],
+            ['value' => 'font-size: 15px; ', 'label' => __('15px')],
+            ['value' => 'font-size: 16px; ', 'label' => __('16px')],
+            ['value' => 'font-size: 17px; ', 'label' => __('17px')],
+            ['value' => 'font-size: 18px; ', 'label' => __('18px')],
+            ['value' => 'font-size: 19px; ', 'label' => __('19px')],
+            ['value' => 'font-size: 20px; ', 'label' => __('20px')]
+        ];
+    }
 }
