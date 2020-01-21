c69e12be6b26b97dfffe29b7c36adbdef0de91df
Ian Gall <ian.gall@scotts.com>
2020-01-20 22:30:24 -0500

    Chagning REM to PX

--- app/code/SMG/SpecsList/Model/Source/HeaderTextSize.php
+++ app/code/SMG/SpecsList/Model/Source/HeaderTextSize.php
@@ -7,17 +7,27 @@ class HeaderTextSize implements \Magento\Framework\Option\ArrayInterface
   public function toOptionArray()
   {
     return [
-      ['value' => 'font-size: 1.0rem; ', 'label' => __('1.0')],
-      ['value' => 'font-size: 1.1rem; ', 'label' => __('1.1')],
-      ['value' => 'font-size: 1.2rem; ', 'label' => __('1.2')],
-      ['value' => 'font-size: 1.3rem; ', 'label' => __('1.3')],
-      ['value' => 'font-size: 1.4rem; ', 'label' => __('1.4')],
-      ['value' => 'font-size: 1.5rem; ', 'label' => __('1.5')],
-      ['value' => 'font-size: 1.6rem; ', 'label' => __('1.6')],
-      ['value' => 'font-size: 1.7rem; ', 'label' => __('1.7')],
-      ['value' => 'font-size: 1.8rem; ', 'label' => __('1.8')],
-      ['value' => 'font-size: 1.9rem; ', 'label' => __('1.9')],
-      ['value' => 'font-size: 2.0rem; ', 'label' => __('2.0')]
+      ['value' => 'font-size: 20px; ', 'label' => __('20px')],
+      ['value' => 'font-size: 21px; ', 'label' => __('21px')],
+      ['value' => 'font-size: 22px; ', 'label' => __('22px')],
+      ['value' => 'font-size: 23px; ', 'label' => __('23px')],
+      ['value' => 'font-size: 24px; ', 'label' => __('24px')],
+      ['value' => 'font-size: 25px; ', 'label' => __('25px')],
+      ['value' => 'font-size: 26px; ', 'label' => __('26px')],
+      ['value' => 'font-size: 27px; ', 'label' => __('27px')],
+      ['value' => 'font-size: 28px; ', 'label' => __('28px')],
+      ['value' => 'font-size: 29px; ', 'label' => __('29px')],
+      ['value' => 'font-size: 30px; ', 'label' => __('30px')],
+      ['value' => 'font-size: 31px; ', 'label' => __('31px')],
+      ['value' => 'font-size: 32px; ', 'label' => __('32px')],
+      ['value' => 'font-size: 33px; ', 'label' => __('33px')],
+      ['value' => 'font-size: 34px; ', 'label' => __('34px')],
+      ['value' => 'font-size: 35px; ', 'label' => __('35px')],
+      ['value' => 'font-size: 36px; ', 'label' => __('36px')],
+      ['value' => 'font-size: 37px; ', 'label' => __('37px')],
+      ['value' => 'font-size: 38px; ', 'label' => __('38px')],
+      ['value' => 'font-size: 39px; ', 'label' => __('39px')],
+      ['value' => 'font-size: 40px; ', 'label' => __('40px')]
     ];
   }
 }
