c334b664743bafce6489f2d5341e47796b2ecbe0
Sean Kegel <sean.kegel@dynamit.com>
2020-01-23 14:42:07 -0500

    Fix quiz

--- app/code/SMG/RecommendationQuiz/view/frontend/web/js/quiz.js
+++ app/code/SMG/RecommendationQuiz/view/frontend/web/js/quiz.js
@@ -508,7 +508,7 @@ define([
         self.loadNextGroup = async group => {
             // No group specified so load the first group.
             if (!group) {
-                self.setGroup(self.template.questionGroups[4]);
+                self.setGroup(self.template.questionGroups[0]);
 
                 return;
             }
