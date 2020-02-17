e7c34fbc458ed5dedfb557f31dab862d45e5bc9c
Nathan VanHoose <hoose145@gmail.com>
2020-02-17 11:31:33 -0500

    Added project to the environment scripts as hotfix is under a new project.

---
+++ environment/scripts/kubernetes-hotfix.sh
@@ -0,0 +1,4 @@
+gcloud config set project ecommerce-platform-hotfix
+gcloud container clusters get-credentials magento-hotfix-ha-cluster --zone us-east1
+instance=$(kubectl get pod -l app=magento-hotfix -o jsonpath="{.items[0].metadata.name}")
+kubectl exec -it $instance -c magento /bin/bash
