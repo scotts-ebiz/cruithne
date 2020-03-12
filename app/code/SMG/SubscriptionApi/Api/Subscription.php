96acdf2fcd1a05c36fe66769b65d29cacd91b63a
nixondev <cnixon@lokion.com>
2020-03-12 15:21:48 -0500

    LS-1510 Api call for subscription data sync with recurly

--- app/code/SMG/SubscriptionApi/Api/Subscription.php
+++ app/code/SMG/SubscriptionApi/Api/Subscription.php
@@ -742,4 +742,14 @@ class Subscription implements SubscriptionInterface
             $this->_cart->removeItem($item->getItemId())->save();
         }
     }
+
+    /**
+     * Get Subscription Data For Data Sync.
+     *
+     * @return array
+     */
+    public function getSubscriptionDataForSync()
+    {
+        return $this->_subscriptionHelper->getSubscriptionDataForSync();
+    }
 }
