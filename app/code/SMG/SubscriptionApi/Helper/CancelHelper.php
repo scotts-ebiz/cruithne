aa1b1248d27684cf15c547200be173115dd8ce3c
nneemkar <neelam.neemkar@scotts.com>
2020-03-15 20:51:08 +0000

    Zaius Cancel Events

--- app/code/SMG/SubscriptionApi/Helper/CancelHelper.php
+++ app/code/SMG/SubscriptionApi/Helper/CancelHelper.php
@@ -11,6 +11,9 @@ use Psr\Log\LoggerInterface;
 use SMG\SubscriptionApi\Model\ResourceModel\Subscription\CollectionFactory as SubscriptionCollectionFactory;
 use Magento\Customer\Model\Session as CustomerSession;
 use Magento\Customer\Model\ResourceModel\Customer\CollectionFactory as CustomerCollectionFactory;
+use SMG\SubscriptionApi\Model\ResourceModel\SubscriptionOrder\CollectionFactory as SubscriptionOrderCollectionFactory;
+use SMG\SubscriptionApi\Model\ResourceModel\SubscriptionOrderItem\CollectionFactory as SubscriptionOrderItemCollectionFactory;
+
 use Zaius\Engage\Helper\Sdk as ZaiusSdk;
 
 class CancelHelper extends AbstractHelper
@@ -44,7 +47,16 @@ class CancelHelper extends AbstractHelper
      * @var SubscriptionCollectionFactory
      */
     protected $_subscriptionCollectionFactory;
-
+    
+	/** @var SubscriptionOrderCollectionFactory */
+    protected $_subscriptionOrderCollectionFactory;
+    
+    /** @var SubscriptionOrderItemCollectionFactory */
+    protected $_subscriptionOrderItemCollectionFactory;
+	
+	/** @var productRepository */
+    protected $_productRepository;
+	
     /**
      * CancelHelper constructor.
      * @param Context $context
@@ -62,7 +74,10 @@ class CancelHelper extends AbstractHelper
         CustomerSession $customerSession,
         SubscriptionCollectionFactory $subscriptionCollectionFactory,
         LoggerInterface $logger,
-        ZaiusSdk $sdk
+        ZaiusSdk $sdk,
+		SubscriptionOrderCollectionFactory $subscriptionOrderCollectionFactory,
+        SubscriptionOrderItemCollectionFactory $subscriptionOrderItemCollectionFactory,
+		\Magento\Catalog\Model\ProductRepository $productRepository
     ) {
         parent::__construct($context);
 
@@ -72,6 +87,9 @@ class CancelHelper extends AbstractHelper
         $this->_subscriptionCollectionFactory = $subscriptionCollectionFactory;
         $this->_logger = $logger;
         $this->_sdk = $sdk;
+		$this->_subscriptionOrderCollectionFactory = $subscriptionOrderCollectionFactory;
+        $this->_subscriptionOrderItemCollectionFactory = $subscriptionOrderItemCollectionFactory;
+		$this->_productRepository = $productRepository;
     }
 
     /**
@@ -99,9 +117,10 @@ class CancelHelper extends AbstractHelper
                 ->fetchItem();
 
             $subscription->cancel();
-
+            $timestamp = strtotime(date("Y-m-d H:i:s"));
             $this->clearCustomerAddresses($customer);
             $this->zaiusCancelCall($customer->getData('email'));
+			$this->zaiusCancelOrder($subscription,$customer->getData('email'),$timestamp);
         } catch (Exception $e) {
             $this->_logger->error($e->getMessage());
 
@@ -162,5 +181,83 @@ class CancelHelper extends AbstractHelper
                 $this->_logger->error("The customer Email Subscription " . $customer_email . " is failed to zaius.");
             }
         }
+    }
+	
+	/**
+     * Cancel subscription notification to zaius
+     * @param $orderdata
+     * @throws \ZaiusSDK\ZaiusException
+     */
+    private function zaiusCancelOrder($subscription,$customer_email,$timestamp)
+    {
+		$zaiusstatus = false;
+
+        // check isSubcription and shipmentstatus
+        if ($subscription) {
+	
+            $subscriptionId = $subscription->getEntityId();	
+            
+			// call subscription order
+			$subscriptionOrders = $this->_subscriptionOrderCollectionFactory->create();
+            $subscriptionOrders
+                ->setOrder('ship_start_date', 'asc')
+                ->addFieldToFilter('subscription_entity_id', $subscriptionId);
+            $this->_subscriptionOrders = $subscriptionOrders;
+			
+            // call getsdkclient function
+            $zaiusClient = $this->_sdk->getSdkClient();
+            // take event as a array and add parameters
+            $event = [];
+			
+            $event['type'] = 'order';
+            $event['action'] = 'cancel';
+            $event['identifiers'] = ['email'=>$customer_email];
+			$event['magento_store_view'] = 'Default Store View';
+			foreach($this->_subscriptionOrders as $orders){
+			$items = [];	
+			$order_id = $orders->getSalesOrderId();
+			$total = $orders->getPrice();
+            $orderItemId = $this->getOrderItems($orders->getEntityId());
+		    
+			foreach ($orderItemId as $item) {
+                 $product = $this->getProductBySku($item->getCatalogProductSku());
+                 $items['product_id'] = $product->getEntityId();
+				 $items['price'] = $item->getPrice();
+				 $items['quantity'] = $item->getQty();
+				 $items['subtotal'] = $items['price'] * $items['quantity'];
+             }
+		    $order = ['order_id'=>$order_id,'total'=>$total,'items'=>[$items]];
+            $event['data'] = ['ts'=>$timestamp,'order'=>$order];
+            // get postevent function
+            $zaiusstatus = $zaiusClient->postEvent($event);
+
+            // check return values from the postevent function
+            if ($zaiusstatus) {
+
+                $this->_logger->debug("The cancel order id" . $order_id . " is cancelled successfully to zaius."); //saved in var/log/debug.log
+            } else {
+
+                $this->_logger->error("The cancel order id" . $order_id . " is failed to zaius.");
+            }
+        }
+	}
+}
+	
+	public function getOrderItems($entity_id)
+    {
+        // Make sure we have an actual subscription
+        if (empty($entity_id)) {
+            return false;
+        }
+        // If subscription orders is local, send them, if not, pull them and send them
+        $subscriptionOrderItems = $this->_subscriptionOrderItemCollectionFactory->create();
+        $subscriptionOrderItems->addFieldToFilter('subscription_order_entity_id', $entity_id);
+        $this->_subscriptionOrderItems = $subscriptionOrderItems;
+
+        return $this->_subscriptionOrderItems;
+    }
+	
+	public function getProductBySku($sku) {
+        return $this->_productRepository->get($sku);
     }
 }
