987f775bfac34a57115358100e72bf5648eb3fd8
Beth Pauley <elizabeth.pauley@scotts.com>
2020-04-01 23:24:16 -0400

    ECOM-1683: Update module version. Since Bonnie will not have its own store at this point, add new custom attribute field to product to indicate it is a Bonnie Product.

--- app/code/SMG/CustomFields/Setup/UpgradeData.php
+++ app/code/SMG/CustomFields/Setup/UpgradeData.php
@@ -303,5 +303,36 @@ class UpgradeData implements UpgradeDataInterface
                 'product_sizes'/* Custom Attribute Code */
             );
         }
+        if (version_compare($context->getVersion(), '1.0.4', '<')) {
+          $eavSetup = $this->eavSetupFactory->create(['setup' => $setup]);
+
+          $eavSetup->addAttribute(
+            \Magento\Catalog\Model\Product::ENTITY,
+            'bonnieflag',
+            [
+              'type' => 'int',
+              'backend' => '',
+              'frontend' => '',
+              'label' => 'Bonnie Product',
+              'input' => 'boolean',
+              'class' => '',
+              'source' => '',
+              'global' => \Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface::SCOPE_GLOBAL,
+              'visible' => true,
+              'required' => false,
+              'user_defined' => false,
+              'default' => '',
+              'searchable' => false,
+              'filterable' => false,
+              'comparable' => false,
+              'visible_on_front' => false,
+              'used_in_product_listing' => true,
+              'unique' => false,
+              'group' => 'Custom Fields',
+              'apply_to' => ''
+            ]
+          );
+
+        }
     }
 }
