1e8492d69d9c22943e4805e3f96e0f6f2939010e
Ian Gall <ian.gall@scotts.com>
2020-04-08 10:29:11 -0400

    1671 - making edits to the crosssell php

--- app/code/SMG/Catalog/Plugin/Crosssell.php
+++ app/code/SMG/Catalog/Plugin/Crosssell.php
@@ -6,53 +6,49 @@ use Psr\Log\LoggerInterface;
 
 class Crosssell extends \Magento\Catalog\Block\Product\ProductList\Crosssell {
 
-  protected $_logger;
+    protected $_logger;
 
-  protected $_productsFactory;
+    protected $_productsFactory;
 
-  protected $_productsResource;
+    protected $_productsResource;
 
+    /* Use Construct to:
+     * Create a Factory Object for ProductFactory
+     * Create a ResourceModel for Products
+     * Create logger var to utilize for debugging as need be
+    */
 
-  /* Use Construct to:
-   * Create a Factory Object for ProductFactory
-   * Create a ResourceModel for Products
-   * Create logger var to utilize for debugging as need be
-  */
+    public function __construct(
+        \Psr\Log\LoggerInterface $logger,
+        \Magento\Catalog\Model\ProductFactory $productsFactory,
+        \Magento\Catalog\Model\ResourceModel\Product $productsResource)
+    {
+        $this->_logger = $logger;
+        $this->_productsFactory = $productsFactory;
+        $this->_productsResource = $productsResource;
+    }
 
-  public function __construct(\Psr\Log\LoggerInterface $logger,
-                              \Magento\Catalog\Model\ProductFactory $productsFactory,
-                              \Magento\Catalog\Model\ResourceModel\Product $productsResource){
-      $this->_logger = $logger;
-      $this->_productsFactory = $productsFactory;
-      $this->_productsResource = $productsResource;
-  }
+    public function afterGetItems(\Magento\Catalog\Block\Product\ProductList\Crosssell $crosssell, $result)
+    {
+        $products = $this->_productsFactory->create();
 
+        //Loop through $results
+        foreach ($result as $crosssellItem) {
+            //Get entity_id
+            $productId = $crosssellItem->getData('entity_id');
 
-  public function afterGetAssociatedProducts(\Magento\GroupedProduct\Block\Product\View\Type\Grouped $subject,
-                                             $result) {
+            /* Use the resource model productResource to:
+             * load all data for specific $productId
+             * place in factory object $products
+            */
+            $this->_productsResource->load($products, $productId);
 
-      $products = $this->_productsFactory->create();
+            //In the factory object $product select the value for 'short_description' and set it to $shortDescription
+            $shortDescription = $products->getData('short_description');
 
-      //Loop through $results
-      foreach ($result as $associatedProductItem) {
-
-
-          //Get entity_id
-          $productId = $associatedProductItem->getData('entity_id');
-
-          /*
-            Use the resource model _productResource to load all data for specific $productId and place in factory
-            object $products
-          */
-          $this->_productsResource->load($products, $productId);
-
-          //In the factory object $product select the value for 'short_description' and set it to $shortDescription
-          $shortDescription = $products->getData('short_description');
-
-          //For each $associatedProductItem take the value of $shortDescription and set it to 'short_description'
-          $associatedProductItem->setData('short_description', $shortDescription);
-      }
-
-    return $result;
-  }
+            //For each $crosssellItem take the value of $shortDescription and set it to 'short_description'
+            $crosssellItem->setData('short_description', $shortDescription);
+        }
+        return $result;
+    }
 }
