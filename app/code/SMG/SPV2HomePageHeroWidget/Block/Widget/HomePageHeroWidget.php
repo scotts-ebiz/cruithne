04d982b7f4c5a2f54cbe4c1b89fc4ef45b65c912
Adam Caouette <adam.caouette@scotts.com>
2020-04-16 13:31:06 -0400

    LS-1217: Update header widget for SP homepage
    Create new widget SPV2HomePageHeroWidget

---
+++ app/code/SMG/SPV2HomePageHeroWidget/Block/Widget/HomePageHeroWidget.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace SMG\SPV2HomePageHeroWidget\Block\Widget;
+
+use Magento\Framework\View\Element\Template;
+use Magento\Widget\Block\BlockInterface;
+
+class HomePageHeroWidget extends Template implements BlockInterface
+{
+    protected $_template = "widget/homepagehero.phtml";
+}
