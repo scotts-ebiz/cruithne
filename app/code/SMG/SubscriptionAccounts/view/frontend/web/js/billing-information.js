9dda705cf4b885b27ceb5d3ebda7b16574054af2
John Gafford <2895943+TheGAFF@users.noreply.github.com>
2020-04-27 08:36:09 -0500

    LS-1754 Update Billing Card Number after saving (#1866)
    Co-authored-by: johngafford <john@johngafford.com>

--- app/code/SMG/SubscriptionAccounts/view/frontend/web/js/billing-information.js
+++ app/code/SMG/SubscriptionAccounts/view/frontend/web/js/billing-information.js
@@ -126,6 +126,10 @@ define([
                         }
                         else {
                             self.billingInfoEditable(false);
+
+                            // Refresh the page to ensure the CC field mask is updated
+                            // since we cannot update it to the new one.
+                            window.location.reload();
                         }
 
                         modalBilling.openModal();
