33be7842aac7a489df36badf5e0cb315140d701e
nneemkar <neelam.neemkar@scotts.com>
2020-05-01 13:39:41 +0000

    Email Opt-n zaius integration

--- app/code/SMG/Checkout/view/frontend/requirejs-config.js
+++ app/code/SMG/Checkout/view/frontend/requirejs-config.js
@@ -4,5 +4,12 @@ var config = {
             ajaxQty: 'SMG_Checkout/js/cartQtyUpdate',
             'Magento_Checkout/js/model/checkout-data-resolver': 'SMG_Checkout/js/model/checkout-data-resolver'
         }
-    }
+    },
+    config: {
+            mixins: {
+                'Magento_Checkout/js/view/shipping': {
+                    'SMG_Checkout/js/view/shipping-mixin': true
+                }
+            }
+        }
 };
