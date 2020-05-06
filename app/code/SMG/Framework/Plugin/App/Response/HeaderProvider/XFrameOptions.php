cb8cf0abe7532ac9d805a06ce23ba2eef0e173bb
John Gafford <2895943+TheGAFF@users.noreply.github.com>
2020-05-06 07:28:08 -0500

    ECOM 1697 Iframe Clickjacking (#1908)
    This PR adds logic to make all requests sameorigin by default except for the iframe pages in order to satisfy the qualys scan reports.
    
    Co-authored-by: johngafford <john@johngafford.com>

--- app/code/SMG/Framework/Plugin/App/Response/HeaderProvider/XFrameOptions.php
+++ app/code/SMG/Framework/Plugin/App/Response/HeaderProvider/XFrameOptions.php
@@ -7,38 +7,40 @@ namespace SMG\Framework\Plugin\App\Response\HeaderProvider;
  */
 class XFrameOptions
 {
-  public function afterGetValue()
-  {
-    $fullDomain = explode('.', $_SERVER['HTTP_HOST']);
-    if (count($fullDomain) < 3) {
-      list($domain, $extension) = explode('.', $_SERVER['HTTP_HOST']);
-    }
-    else {
-      list($subdomain, $domain, $extension) = explode('.', $_SERVER['HTTP_HOST']);
-    }
-    // For older browsers without [X-]Content-Security-Policy support.
-    // Only ONE domain (no wildcard) allowed. So determine specific allowed domain.
-    // Newer browsers should IGNORE this if Content-Sec-Policy header also present.
-    $xfValue = 'SAMEORIGIN';
-    if ($subdomain === 'store' || $subdomain === 'shop') {
-      $xfValue = 'ALLOW-FROM https://www.' . $domain . '.com/';
-    }
-    else if ($subdomain === 'rc') {
-      $xfValue = ' ALLOW-FROM https://acsftest.' . $domain . '.com/';
-    }
-    else if ($subdomain === 'staging') {
-      $xfValue = ' ALLOW-FROM https://acsfdev.' . $domain . '.com/';
-    }
-    else if ($subdomain === 'test') {
-      $xfValue = 'ALLOW-FROM https://acsfdev.' . $domain . '.com/';
-    }
-    else if ($subdomain === 'dev') {
-      $xfValue = 'ALLOW-FROM https://acsfdev.' . $domain . '.com/';
-    }
-    else if ($subdomain === 'cruithne' && $extension === 'local') {
-      $xfValue = 'ALLOW-FROM https://' . $domain . '.test/';
-    }
+    public function afterGetValue()
+    {
+        $fullDomain = explode('.', $_SERVER['HTTP_HOST']);
+        if (count($fullDomain) < 3) {
+            list($domain, $extension) = explode('.', $_SERVER['HTTP_HOST']);
+        } else {
+            list($subdomain, $domain, $extension) = explode('.', $_SERVER['HTTP_HOST']);
+        }
 
-    return $xfValue;
-  }
+        $xfValue = 'SAMEORIGIN';
+
+        // If we aren't serving up an iframe, then return sameorigin to satisfy security scans.
+        $urlPath = parse_url($_SERVER["REQUEST_URI"])['path'];
+        if (strpos($urlPath, 'iframes') === FALSE) {
+            return $xfValue;
+        }
+        
+        // For older browsers without [X-]Content-Security-Policy support.
+        // Only ONE domain (no wildcard) allowed. So determine specific allowed domain.
+        // Newer browsers should IGNORE this if Content-Sec-Policy header also present.
+        if ($subdomain === 'store' || $subdomain === 'shop') {
+            $xfValue = 'ALLOW-FROM https://www.' . $domain . '.com/';
+        } elseif ($subdomain === 'rc') {
+            $xfValue = ' ALLOW-FROM https://acsftest.' . $domain . '.com/';
+        } elseif ($subdomain === 'staging') {
+            $xfValue = ' ALLOW-FROM https://acsfdev.' . $domain . '.com/';
+        } elseif ($subdomain === 'test') {
+            $xfValue = 'ALLOW-FROM https://acsfdev.' . $domain . '.com/';
+        } elseif ($subdomain === 'dev') {
+            $xfValue = 'ALLOW-FROM https://acsfdev.' . $domain . '.com/';
+        } elseif ($subdomain === 'cruithne' && $extension === 'local') {
+            $xfValue = 'ALLOW-FROM https://' . $domain . '.test/';
+        }
+
+        return $xfValue;
+    }
 }
