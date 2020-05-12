1e1e5ff13e5e77e1bcabeba65ebe17ef8fc7725b
Ian Gall <ian.gall@scotts.com>
2020-05-12 08:10:43 -0400

    1434 - Removing auto formating to total js page

--- app/design/frontend/Scottsbase/scottsprogram-v2/Magento_Checkout/web/js/view/summary/totals.js
+++ app/design/frontend/Scottsbase/scottsprogram-v2/Magento_Checkout/web/js/view/summary/totals.js
@@ -1,63 +1,51 @@
-define(["ko", "Magento_Checkout/js/view/summary/abstract-total"], function (
-	ko,
-	Component
-) {
-	"use strict";
+define([
+    'ko',
+    'Magento_Checkout/js/view/summary/abstract-total'
+], function (ko, Component) {
+    'use strict';
 
-	return Component.extend({
-		initialize() {
-			this._super();
+    return Component.extend({
+        initialize() {
+            this._super();
 
-			this.subscriptionData = ko.observable(window.subscriptionData);
+            this.subscriptionData = ko.observable(window.subscriptionData);
 
-			this.recurlyApiObserve = ko.observable(window.recurlyApi);
+            const summaryInterval = setInterval(() => {
+                let summaryContainer = document.querySelector('.opc-block-summary')
+                if(summaryContainer) {
+                    
+                    const toggle = document.createElement('span');
+                    toggle.classList.add('toggle-arrow');
+                    summaryContainer.querySelector('.title').appendChild(toggle);
 
-			const summaryInterval = setInterval(() => {
-				let summaryContainer = document.querySelector(
-					".opc-block-summary"
-				);
-				if (summaryContainer) {
-					const toggle = document.createElement("span");
-					toggle.classList.add("toggle-arrow");
-					summaryContainer
-						.querySelector(".title")
-						.appendChild(toggle);
+                    toggle.addEventListener('click', this.toggleSummary.bind(null, summaryContainer));
 
-					toggle.addEventListener(
-						"click",
-						this.toggleSummary.bind(null, summaryContainer)
-					);
+                    this.summary = summaryContainer;
+                    clearInterval(summaryInterval);
+                }
+            }, 100);
 
-					this.summary = summaryContainer;
-					clearInterval(summaryInterval);
-				}
-			}, 100);
-		},
+        },
 
-		/**
-		 * @return {*}
-		 */
-		isDisplayed: function () {
-			return this.isFullMode();
-		},
+        /**
+         * @return {*}
+         */
+        isDisplayed: function () {
+            return this.isFullMode();
+        },
 
-		isTaxDisplayedInGrandTotal: function () {
-			return window.checkoutConfig.includeTaxInGrandTotal;
-		},
+        isTaxDisplayedInGrandTotal: function() {
+            return window.checkoutConfig.includeTaxInGrandTotal;
+        },
 
-		getGrandTotal: function () {
-			const found = this.elems().filter(
-				(elem) =>
-					elem.name === "checkout.sidebar.summary.totals.grand-total"
-			);
-			return found ? found[0].getValue() : 0;
-		},
+        getGrandTotal: function() {
+            const found = this.elems().filter(elem => elem.name === 'checkout.sidebar.summary.totals.grand-total');
+            return found ? found[0].getValue() : 0;
+        },
 
-		toggleSummary: function (summary) {
-			let active = summary.classList.contains("active");
-			active
-				? summary.classList.remove("active")
-				: summary.classList.add("active");
-		},
-	});
+        toggleSummary: function(summary) {
+            let active = summary.classList.contains('active');
+            active ? summary.classList.remove('active') : summary.classList.add('active');
+        }
+    });
 });
