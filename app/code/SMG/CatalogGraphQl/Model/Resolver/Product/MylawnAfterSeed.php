9598084ffd38c80524b56b7d1f1c91576044d5a1
nixondev <cnixon@lokion.com>
2020-10-11 11:49:13 -0500

    COM-997 added resolvers for PIM select properties

---
+++ app/code/SMG/CatalogGraphQl/Model/Resolver/Product/MylawnAfterSeed.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace SMG\CatalogGraphQl\Model\Resolver\Product;
+
+use Magento\Catalog\Model\Product;
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Framework\GraphQl\Config\Element\Field;
+use Magento\Framework\GraphQl\Query\ResolverInterface;
+use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
+
+/**
+ * Resolve data
+ */
+class MylawnAfterSeed implements ResolverInterface
+{
+    public function resolve(
+        Field $field,
+        $context,
+        ResolveInfo $info,
+        array $value = null,
+        array $args = null
+    )
+    {
+        if (!isset($value['model'])) {
+            throw new LocalizedException(__('"model" value should be specified'));
+        }
+
+        /* @var $product Product */
+        $product = $value['model'];
+        $return = array();
+        $attribute = $product->getAttributeText('mylawn_after_seed');
+
+        return ($attribute) ? array_merge((array)$attribute, $return): $return;
+    }
+}
