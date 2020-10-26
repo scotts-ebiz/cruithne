b10d26f4739e1fd6f8123d05153d9764824eb6ff
Niket Kanoi <niket.kanoi@scotts.com>
2020-10-26 08:49:05 -0500

    COM-830 - Credit Memo CSR functions

--- app/code/SMG/BackendService/Plugin/AfterCreateCreditMemo.php
+++ app/code/SMG/BackendService/Plugin/AfterCreateCreditMemo.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace SMG\BackendService\Plugin;
 
 use SMG\BackendService\Model\Client\Api;
@@ -49,9 +50,7 @@ class AfterCreateCreditMemo
         $result
     ) {
         $order = $this->registry->registry('current_creditmemo');
-        
-      //check module status active or not
-       if($this->config->getStatus()){
+        if ($order) {
 
             $response = $this->client->execute(
                 $this->config->getOrderApiUrl(),
@@ -59,9 +58,8 @@ class AfterCreateCreditMemo
                 $this->buildOrderObject($order),
                 Request::HTTP_METHOD_POST
             );
-            
-        }
 
+        }
         return $result;
     }
 
@@ -69,14 +67,14 @@ class AfterCreateCreditMemo
      * @param $order
      * @return array
      */
-    public function buildOrderObject($order) {
-
+    public function buildOrderObject($order)
+    {
         $params = [];
         $params['transId'] = $this->config->generateUuid();
         $params['sourceService'] = $this->config->getWebSource();
         $params['orderId'] = $order->getOrderId();
         $params['noteType'] = 'email';
-        $params['noteMessage'] = 'Credit memo successfully created';
+        $params['noteMessage'] = $order->getCustomerNote();
         $params['condition'] = 'success';
 
         return $params;
