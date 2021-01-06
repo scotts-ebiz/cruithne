1b58f388f56898143112f47e9ddc7a2275620785
nixondev <cnixon@lokion.com>
2021-01-06 16:01:47 -0600

    COM-1158 To help with module update from this workstream to original

--- app/code/SMG/SubscriptionApi/Setup/UpgradeData.php
+++ app/code/SMG/SubscriptionApi/Setup/UpgradeData.php
@@ -179,6 +179,14 @@ class UpgradeData implements UpgradeDataInterface
      */
     private function addDataVersion116(ModuleDataSetupInterface $setup)
     {
+        // Upgrade Subscription Status
+        $tableName = $setup->getTable('subscription_status');
+
+        $data = [
+            ['status' => 'renewed', 'label' => 'Renewed']
+        ];
+
+        $setup->getConnection()->insertMultiple($tableName, $data);
         // Do nothing. Done purposefully to resolve merge.
     }
 
@@ -230,13 +238,6 @@ class UpgradeData implements UpgradeDataInterface
      */
     private function addDataVersion119(ModuleDataSetupInterface $setup)
     {
-        // Upgrade Subscription Status
-        $tableName = $setup->getTable('subscription_status');
-
-        $data = [
-            ['status' => 'renewed', 'label' => 'Renewed']
-        ];
-
-        $setup->getConnection()->insertMultiple($tableName, $data);
+        // Do nothing. Done purposefully to resolve merge.
     }
 }
