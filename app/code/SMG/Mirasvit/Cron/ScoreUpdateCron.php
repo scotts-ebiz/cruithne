cc0ae775f331f500d9af7366396f95ea54c77889
Chad Nixon <chad.nixon@scotts.com>
2021-06-29 21:25:17 +0000

    COM-1476 invoice all accepted orders during Mirasvit Fraud cron process

--- app/code/SMG/Mirasvit/Cron/ScoreUpdateCron.php
+++ app/code/SMG/Mirasvit/Cron/ScoreUpdateCron.php
@@ -2,13 +2,21 @@
 
 namespace SMG\Mirasvit\Cron;
 
+use Magento\Sales\Model\Order;
 use Magento\Sales\Model\ResourceModel\Order\CollectionFactory as OrderCollectionFactory;
+use Mirasvit\FraudCheck\Model\Score;
 use Mirasvit\FraudCheck\Model\ScoreFactory;
 use Magento\Framework\App\Config\ScopeConfigInterface;
+use SMG\Api\Helper\OrderStatusHelper;
 
 class ScoreUpdateCron
 {
     const XML_FRAUD_CHECK_CONFIG_CRON_PATH = 'fraud_check/cron/';
+
+    /**
+     * @var OrderStatusHelper
+     */
+    protected $_orderStatusHelper;
 
     /**
      * @var OrderCollectionFactory
@@ -30,15 +38,18 @@ class ScoreUpdateCron
      * @param OrderCollectionFactory $orderCollectionFactory
      * @param ScoreFactory $scoreFactory
      * @param ScopeConfigInterface $scopeConfigInterface
+     * @param OrderStatusHelper $orderStatusHelper
      */
     public function __construct(
         OrderCollectionFactory $orderCollectionFactory,
         ScoreFactory $scoreFactory,
-        ScopeConfigInterface $scopeConfigInterface
+        ScopeConfigInterface $scopeConfigInterface,
+        OrderStatusHelper $orderStatusHelper
     ) {
         $this->orderCollectionFactory = $orderCollectionFactory;
         $this->scoreFactory           = $scoreFactory;
         $this->scopeConfigInterface   = $scopeConfigInterface;
+        $this->_orderStatusHelper = $orderStatusHelper;
     }
 
     /**
@@ -70,6 +81,23 @@ class ScoreUpdateCron
                     $score->setOrder($order);
                 } catch (\Exception $e) {
                     // skip score update for that order
+                }
+            }
+            /** @var Order $order */
+            foreach ($collection as $order) {
+                $fraudStatus = $order->getData('fraud_status');
+                if ($fraudStatus === Score::STATUS_APPROVE) {
+                    try {
+                        $this->_orderStatusHelper->createInvoice($order);
+                    } catch (\Exception | \Throwable $e ) {
+                        try {
+                            if ($order->canHold()) {
+                                $order->hold();
+                            }
+                        } catch (\Exception $e) {
+                            // I give up
+                        }
+                    }
                 }
             }
         }
