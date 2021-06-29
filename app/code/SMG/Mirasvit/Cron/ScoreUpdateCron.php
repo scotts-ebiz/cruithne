fb17ceebf4c1fe0832ae391118f1e90d43b42652
nixondev <cnixon@lokion.com>
2021-06-29 14:24:15 -0500

    COM-1476 invoice all accepted orders during Mirasvit Fraud cron process

--- app/code/SMG/Mirasvit/Cron/ScoreUpdateCron.php
+++ app/code/SMG/Mirasvit/Cron/ScoreUpdateCron.php
@@ -2,13 +2,23 @@
 
 namespace SMG\Mirasvit\Cron;
 
+use Magento\Sales\Model\Order;
 use Magento\Sales\Model\ResourceModel\Order\CollectionFactory as OrderCollectionFactory;
+use Mirasvit\FraudCheck\Model\Score;
 use Mirasvit\FraudCheck\Model\ScoreFactory;
 use Magento\Framework\App\Config\ScopeConfigInterface;
+use SMG\Api\Helper\OrderStatusHelper;
+use SMG\Sap\Model\SapOrderBatch;
+use SMG\Sap\Model\SapOrderBatchFactory;
 
 class ScoreUpdateCron
 {
     const XML_FRAUD_CHECK_CONFIG_CRON_PATH = 'fraud_check/cron/';
+
+    /**
+     * @var OrderStatusHelper
+     */
+    protected $_orderStatusHelper;
 
     /**
      * @var OrderCollectionFactory
@@ -19,6 +29,11 @@ class ScoreUpdateCron
      * @var ScoreFactory
      */
     private $scoreFactory;
+
+    /**
+     * @var SapOrderBatchFactory
+     */
+    protected $_sapOrderBatchFactory;
 
     /**
      * @var ScopeConfigInterface
@@ -34,11 +49,15 @@ class ScoreUpdateCron
     public function __construct(
         OrderCollectionFactory $orderCollectionFactory,
         ScoreFactory $scoreFactory,
-        ScopeConfigInterface $scopeConfigInterface
+        ScopeConfigInterface $scopeConfigInterface,
+        SapOrderBatchFactory $sapOrderBatchFactory,
+        OrderStatusHelper $orderStatusHelper
     ) {
         $this->orderCollectionFactory = $orderCollectionFactory;
         $this->scoreFactory           = $scoreFactory;
         $this->scopeConfigInterface   = $scopeConfigInterface;
+        $this->_sapOrderBatchFactory = $sapOrderBatchFactory;
+        $this->_orderStatusHelper = $orderStatusHelper;
     }
 
     /**
@@ -70,6 +89,23 @@ class ScoreUpdateCron
                     $score->setOrder($order);
                 } catch (\Exception $e) {
                     // skip score update for that order
+                }
+            }
+            /** @var Order $order */
+            foreach ($collection as $order) {
+                $fraudStatus = $order->getData('fraud_status');
+                if ($fraudStatus === Score::STATUS_APPROVE) {
+                    try {
+                        $this->_orderStatusHelper->createInvoice($order);
+                    } catch (\Exception | \Throwable $e ) {
+                        try {
+                            if ($order->canHold()) {
+                                $order->hold();
+                            }
+                        } catch (\Exception $e) {
+                            // I give up
+                        }
+                    }
                 }
             }
         }
