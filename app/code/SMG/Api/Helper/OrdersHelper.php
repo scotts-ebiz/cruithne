9c3d9ee6c69119a474c8e77a9870533e28b78a0f
Chad Nixon <chad.nixon@scotts.com>
2022-04-11 17:10:00 +0000

    COM-1603

--- app/code/SMG/Api/Helper/OrdersHelper.php
+++ app/code/SMG/Api/Helper/OrdersHelper.php
@@ -807,7 +807,7 @@ class OrdersHelper
         }
 
         //COM-1603 Negative invoice amount being sent to SAP when coupon exceeds price
-        if ($invoiceAmount == false || $invoiceAmount < 0) {
+        if (floatval($invoiceAmount) <= 0) {
             $invoiceAmount = 0;
             $hdrDiscCondCode = 'Z616';
             $hdrDiscPerc = '100';
